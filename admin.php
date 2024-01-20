2863a45feb04a2b17188bf5a59a61675b41cb94b
Avish Madushanka <avishmadushanka21@gmail.com>
2024-01-20 13:48:57 +0530

    Create admin.php

---
+++ admin.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>About Us</title>
+<link rel="icon" type="image/x-icon" href="/image/Drivex.png">
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="description" content="DriveX">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="styles/bootstrap4/bootstrap.min.css">
+<link href="plugins/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.2.1/owl.carousel.css">
+<link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.2.1/owl.theme.default.css">
+<link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.2.1/animate.css">
+<link rel="stylesheet" type="text/css" href="styles/about_styles.css">
+<link rel="stylesheet" type="text/css" href="styles/about_responsive.css">
+</head>
+<style>
+   
+	<?php include "styles/about_styles.css" ?>
+	<?php include "styles/about_responsive.css" ?>
+	<?php include "plugins/OwlCarousel2-2.2.1/animate.css" ?>
+	<?php include "plugins/OwlCarousel2-2.2.1/owl.theme.default.css" ?>
+	<?php include "plugins/OwlCarousel2-2.2.1/owl.carousel.css" ?>
+  </style>
+  </head>
+  
+  <body>
+  
+
+
+	<?php include "header.php" ?>
+
+	<!-- Home -->
+
+	<div class="home">
+		<div class="home_background parallax-window" data-parallax="scroll" data-image-src="image/nsbm2.jpg"></div>
+		<div class="home_content">
+			<div class="home_title">about us</div>
+		</div>
+	</div>
+
+	<!-- Intro -->
+
+	<div class="intro">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-7">
+					<div class="intro_image"><img src="image/nsbmlogo.jpg" alt=""></div>
+				</div>
+				<div class="col-lg-5">
+					<div class="intro_content">
+						<div class="intro_title">NSBM Green University</div>
+						<p class="intro_text">NSBM Green University, the nation’s premier degree-awarding institute, is the first of its kind in South Asia. It is a government-owned self-financed institute that operates under the purview of the Ministry of Education. As a leading educational centre in the country, NSBM has evolved into becoming a highly responsible higher education institute that offers unique opportunities and holistic education on par with international standards while promoting sustainable living. 
+							NSBM offers a plethora of undergraduate and postgraduate degree programmes under five faculties: Business, Computing, Engineering, Science and Postgraduate & Professional Advancement. These study programmes at NSBM are either its own programmes recognised by the University Grants Commission and the Ministry of Higher Education or world-class international programmes conducted in affiliation with top-ranked foreign universities such as University of Plymouth, UK, and Victoria University, Australia.  
+							</p>
+						<div><div></div><span></span><span></span><span></span></a></div>
+					</div>
+				</div>
+				<p class="nsbm">Focused on producing competent professionals and innovative entrepreneurs for the increasingly globalising world, NSBM nurtures its graduates to become productive citizens of society with their specialisation ranging in study fields such as Business, Management, Computing, IT, Engineering, Science, Psychology, Nursing, Interior design, Quantity Surveying, Law and Multimedia. 
+					Inspired by the vision of being Sri Lanka’s best-performing graduate school and being recognised internationally, NSBM currently achieves approximately 3000 new enrollments per year and houses above 11,000 students reading over 50 degree programmes under 4 faculties. Moreover, over the years, NSBM Green University has gifted the nation with 14,000+ graduates and has proved its global presence with an alumni network spread all across the world. 
+					Nestling on a 40-acre land amidst the greenery and serenity in Pitipana, Homagama, NSBM Green University, is an ultra-modern university complex constructed with state-of-the-art facilities and amenities that provides the perfect setting for high-quality teaching, learning and research.</p>
+
+			</div>
+		</div>
+	</div>
+
+	
+
+
+	<div class="add">
+		<div class="container">
+			<div class="row">
+				<div class="col">
+					<div class="add_container">
+						<div class="add_background" style="background-image:url(image/nsbm.jpg)"></div>
+						<center>
+						<div class="button add_button"><div class="button_bcg"></div><a href="https://www.nsbm.ac.lk/">see more details</a><div></div><span></span><span></span><span></span></a></div>
+					    </center>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+<br><br><br><br><br><br><br><br><br><br><br>
+	
+	
+
+	
+	
+	<!-- Footer -->
+	
+	<?php include "footer.php" ?>
+
+
+<script src="js/jquery-3.2.1.min.js"></script>
+<script src="styles/bootstrap4/popper.js"></script>
+<script src="styles/bootstrap4/bootstrap.min.js"></script>
+<script src="plugins/greensock/TweenMax.min.js"></script>
+<script src="plugins/greensock/TimelineMax.min.js"></script>
+<script src="plugins/scrollmagic/ScrollMagic.min.js"></script>
+<script src="plugins/greensock/animation.gsap.min.js"></script>
+<script src="plugins/greensock/ScrollToPlugin.min.js"></script>
+<script src="plugins/OwlCarousel2-2.2.1/owl.carousel.js"></script>
+<script src="plugins/easing/easing.js"></script>
+<script src="plugins/parallax-js-master/parallax.min.js"></script>
+<script src="js/about_custom.js"></script>
+
+</body>
+
+</html>
