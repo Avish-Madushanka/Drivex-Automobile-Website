75233c386a8a658b21f97957ed5ef41d78923a54
Avish Madushanka <avishmadushanka21@gmail.com>
2024-01-21 13:24:39 +0530

    Update dbconnection.php

--- admin/dbconnection.php
+++ admin/dbconnection.php
@@ -1,1 +1,7 @@
-
+<?php
+$con=mysqli_connect("localhost", "drivvnet_drivex", "Drivex12345!!", "drivvnet_drivex");
+if(mysqli_connect_errno())
+{
+echo "Connection Fail".mysqli_connect_error();
+}
+?>
