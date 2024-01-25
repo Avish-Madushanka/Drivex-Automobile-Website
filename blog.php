85cf1504ce74dca3e033896cf468dc58aa8a8462
Avish Madushanka <avishmadushanka21@gmail.com>
2024-01-25 21:06:27 +0530

    Update blog.php

--- blog.php
+++ blog.php
@@ -1,1 +1,240 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Blog</title>
+<link rel="icon" type="image/x-icon" href="/image/Drivex.png">
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="description" content="DriveX">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="styles/bootstrap4/bootstrap.min.css">
+<link href="plugins/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<link href="plugins/colorbox/colorbox.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" type="text/css" href="styles/blog_styles.css">
+<link rel="stylesheet" type="text/css" href="styles/blog_responsive.css">
+</head>
 
+<style>
+   
+	<?php include "styles/contact_styles.css" ?>
+	<?php include "styles/contact_responsive.css" ?>
+	<?php include "plugins/OwlCarousel2-2.2.1/animate.css" ?>
+	<?php include "plugins/OwlCarousel2-2.2.1/owl.theme.default.css" ?>
+	<?php include "plugins/OwlCarousel2-2.2.1/owl.carousel.css" ?>
+  </style>
+  </head>
+  
+  <body>
+  
+  <?php include "header.php" ?>
+  
+	<!-- Home -->
+
+	<div class="home">
+		<div class="home_background parallax-window" data-parallax="scroll" data-image-src="image/homeblog.jpg"></div>
+		<div class="home_content">
+			<div class="home_title">the blog</div>
+		</div>
+	</div>
+
+	<!-- Blog -->
+
+	<div class="blog">
+		<div class="container">
+			<div class="row">
+
+				<!-- Blog Content -->
+
+				<div class="col-lg-8">
+					
+					<div class="blog_post_container">
+
+						<!-- Blog Post -->
+						
+						<div class="blog_post">
+							<div class="blog_post_image">
+								<img src="image/bmwblog.jpg" >
+								<div class="blog_post_date d-flex flex-column align-items-center justify-content-center">
+									<div class="blog_post_day">20</div>
+									<div class="blog_post_month">Nov, 2022</div>
+								</div>
+							</div>
+							<div class="blog_post_meta">
+								<ul>
+									<li class="blog_post_meta_item">By Parindya Liyanage</li>
+									
+								</ul>
+							</div>
+							<div class="blog_post_title"><h2 class="tit">BMW says keeping your old car is better than buying a new car…</h2></div>
+							<div class="blog_post_text">
+								<p>BMW has large portfolio, which includes models that suit different needs like sports cars, family SUVs and luxury sedans. With constant hardware and software upgrades, the automaker always keeps its range fresh and competitive, which basically means there’s always a good new BMW you can buy, if you are ready to pay the price for it. But it turns out, the company doesn’t want you to buy a new car at any cost and that’s not very good for the environment.
+									The idea comes straight from the firm’s executives Monika Dernai, BMW’s head of sustainability. She recently spoke t an audience in London about how the automotive industry could reduce the environmental impact and waste. Her idea is to keep fresh and modern the vehicles that are currently in use but are getting older by retrofitting them upgrades instead of just picking up a car from the showroom.
+									</p>
+							</div>
+							
+						</div>
+
+						<!-- Blog Post -->
+						
+						<div class="blog_post">
+							<div class="blog_post_image">
+								<img src="image/mazdablog.jpg">
+								<div class="blog_post_date d-flex flex-column align-items-center justify-content-center">
+									<div class="blog_post_day">26</div>
+									<div class="blog_post_month">Nov, 2022</div>
+								</div>
+							</div>
+							<div class="blog_post_meta">
+								<ul>
+									<li class="blog_post_meta_item">By Parindya Liyanage</li>
+									
+								</ul>
+							</div>
+							<div class="blog_post_title"><h2 class="tit">MAZDA has made the MX-5 Miata for people with disabilities…</h2></div>
+							<div class="blog_post_text">
+								<p>Now, the Mazda MX-5 is more disability-friendly a than it was previous with a few modifications and some new special features. This new development will be a significant help to people with disabilities who would have trouble operating pedals on a standard vehicle with just two hand and without feet. </p>
+								<p>At the beginning of October 2022, Mazda showed a vehicle called “Self-empowerment Driving Vehicle” that has a manual driving device. Mazda says these vehicles are ones with the mentioned above device that allow drivers “to move and act on their own will".</p>
+								<p>There are few new parts incorporated into operating the vehicle. A level break is for achieving manual operation, and accelerator ring is present to intuitively accelerate and decelerate by hand. Drivers can rotate who is driving among the passengers, or they can simply choose between using the pedals like a standard car or activating the manual operation option.</p>
+								
+							</div>
+							
+						</div>
+						<!-- Blog Post -->
+						
+						<div class="blog_post">
+							<div class="blog_post_image">
+								<img src="image/porscheblog.jpg" >
+								<div class="blog_post_date d-flex flex-column align-items-center justify-content-center">
+									<div class="blog_post_day">30</div>
+									<div class="blog_post_month">Nov, 2022</div>
+								</div>
+							</div>
+							<div class="blog_post_meta">
+								<ul>
+									<li class="blog_post_meta_item">By Parindya Liyanage</li>
+									
+								</ul>
+							</div>
+							<div class="blog_post_title"><h2 class="tit">BMW says keeping your old car is better than buying a new car…</h2></div>
+							<div class="blog_post_text">
+								<p>To celebrate the brand’s 70th year selling cars in North America, Porsche has put together an off-road Dakar version of the 911 with knobby tires, a lifted suspension and Carrera 4 GTS-like powertrains.
+									Only 2500 will be made and the starting price is $223,450.
+									</p>
+								<p>Porsche says the Carrera T weight 100 pounds less than the base model despite the extra equipment. Weight saving measures include deleting the rear seat, reduced sound deadening, a small battery, and thinner window glass.</p>
+								<p>The seats are comfortable and supportive with bolstering suitable for hard driving but not uncomfortable for daily use. Extra-aggressive carbon-fiber racing-style seats are also available.</p>
+								<p>Although the Carrera T is a thoroughly modern car, it captures the spirit of the 60s original.</p>
+							</div>
+							
+						</div>
+
+						<!-- Blog Post -->
+						
+						<div class="blog_post">
+							<div class="blog_post_image">
+								<img src="image/electricblog.jpg" alt="https://unsplash.com/@stilclassics">
+								<div class="blog_post_date d-flex flex-column align-items-center justify-content-center">
+									<div class="blog_post_day">30</div>
+									<div class="blog_post_month">Nov, 2022</div>
+								</div>
+							</div>
+							<div class="blog_post_meta">
+								<ul>
+									<li class="blog_post_meta_item">By Parindya Liyange</li>
+									
+								</ul>
+							</div>
+							<div class="blog_post_title"><h2 class="tit">Electric vehicles are saving the climate…</h2></div>
+							<div class="blog_post_text">
+								<p>There is a myth that electric vehicles are just as bad or the climate and worse than the gas-powered cars and trucks. One common myth is that the climate warming pollution caused by manufacturing electric vehicle batteries cancels out the benefits. That’s wrong.</p>
+								<p>EVs are responsible for less global warming pollution over their life cycle than gas powered vehicles. Some studies have shown that making typical electric vehicle can create more carbon pollution than making gasoline car. This is because of the additional energy required to manufacture an EV’s battery. Still, over the lifetime of the vehicle, total greenhouse gas emission associated with manufacturing, charging, and driving an EV are typically lower than the total GHGs associated with a gasoline car.</p>
+								
+							</div>
+							
+						</div>
+
+					</div>
+						
+					
+				</div>
+
+				<!-- Blog Sidebar -->
+
+				<div class="col-lg-4 sidebar_col">
+
+					
+
+					<!-- Sidebar Gallery -->
+					<div class="sidebar_gallery">
+						<div class="sidebar_title">GALLERY</div>
+						<div class="gallery_container">
+							<ul class="gallery_items d-flex flex-row align-items-start justify-content-between flex-wrap">
+								<li class="gallery_item">
+									
+										<img src="image/audispR8.jfif" width=250 height=80>
+									</a>
+								</li>
+								<li class="gallery_item">
+								
+										<img src="image/blogN.jpg"width=250 height=80 >
+									</a>
+								</li>
+								<li class="gallery_item">
+									
+										<img src="image/bugattich.jpg" width=250 height=80>
+									</a>
+								</li>
+								<li class="gallery_item">
+								
+										<img src="image/clbenz24.jpg" width=250 height=80 >
+									</a>
+								</li>
+								<li class="gallery_item">
+									
+										<img src="image/clporsche914.jpg"  width=250 height=80>
+									</a>
+								</li>
+								<li class="gallery_item">
+								
+										<img src="image/clbmw78.jpg"  width=250 height=80>
+									</a>
+								</li>
+								<li class="gallery_item">
+									
+										<img src="image/i8.jpg" width=250 height=80>
+									</a>
+								</li>
+								<li class="gallery_item">
+								
+										<img src="image/jaguf.jpg"width=250 height=80 >
+									</a>
+								</li>
+								<li class="gallery_item">
+									
+										<img src="image/spskyline.jpg" width=250 height=80>
+									</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!-- Footer -->
+	
+	<?php include "footer.php" ?>
+
+</div>
+
+<script src="js/jquery-3.2.1.min.js"></script>
+<script src="styles/bootstrap4/popper.js"></script>
+<script src="styles/bootstrap4/bootstrap.min.js"></script>
+<script src="plugins/colorbox/jquery.colorbox-min.js"></script>
+<script src="plugins/parallax-js-master/parallax.min.js"></script>
+<script src="js/blog_custom.js"></script>
+
+</body>
+
+</html>
