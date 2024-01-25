ac65442b34862d8cd85fa097f0681ada54fa66ce
Avish Madushanka <avishmadushanka21@gmail.com>
2024-01-25 21:08:11 +0530

    Update feedbackform.php

--- feedbackform.php
+++ feedbackform.php
@@ -1,1 +1,54 @@
+<?php 
 
+
+
+
+$name               = $_POST['name'];
+$email              = $_POST['email'];
+$subject            = $_POST['subject'];
+$feedback           = $_POST['feedback'];
+
+
+
+
+
+
+
+
+//create connection
+
+$conn=mysqli_connect("localhost", "drivvnet_drivex", "Drivex12345!!", "drivvnet_drivex");
+
+if (isset($_POST['submit'])) {
+    $sql = "INSERT INTO tbl_feedback(
+    name,
+    email,
+    subject,
+    feedback
+
+
+    )
+
+
+
+VALUES(
+    '$name',
+    '$email',           
+    '$subject',
+    '$feedback'
+
+)";
+
+
+
+
+
+ if ($conn->query($sql) === TRUE) {
+        echo "<script>alert('You have successfully inserted the data');</script>";
+echo "<script type='text/javascript'> document.location ='index.php'; </script>";
+} else{
+echo "<script>alert('Something Went Wrong. Please try again');</script>";
+}
+}
+
+?>
