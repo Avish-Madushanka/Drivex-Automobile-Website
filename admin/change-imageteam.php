b6e6d1e105c96ee0650ba341b9de68809c488520
Avish Madushanka <avishmadushanka21@gmail.com>
2024-02-01 21:07:11 +0530

    Update change-imageteam.php

--- admin/change-imageteam.php
+++ admin/change-imageteam.php
@@ -1,1 +1,179 @@
+<?php 
+//Database Connection
+include('dbconnection.php');
+if(isset($_POST['submit']))
+  {
+  	$uid=$_GET['userid'];
+  	//getting the post values
+  $ppic=$_FILES["profilepic"]["name"];
+   $oldppic=$_POST['oldpic'];
+$oldprofilepic="profilepics"."/".$oldppic;
+// get the image extension
+$extension = substr($ppic,strlen($ppic)-4,strlen($ppic));
+// allowed extensions
+$allowed_extensions = array(".jpg","jpeg",".png",".gif");
+// Validation for allowed extensions .in_array() function searches an array for a specific value.
+if(!in_array($extension,$allowed_extensions))
+{
+echo "<script>alert('Invalid format. Only jpg / jpeg/ png /gif format allowed');</script>";
+}
+else
+{
+//rename the image file
+$imgnewfile=md5($oldppic).time().$extension;
+// Code for move image into directory
+move_uploaded_file($_FILES["profilepic"]["tmp_name"],"profilepics/".$imgnewfile);
+  // Query for data insertion
+     $query=mysqli_query($con, "update team set ProfilePic='$imgnewfile' where id='$uid' ");
+    if ($query) {
+    	//Old pic
+    	unlink($oldprofilepic);
+    echo "<script>alert('Profile pic updated successfully');</script>";
+    echo "<script type='text/javascript'> document.location ='indexteam.php'; </script>";
+  }
+  else
+    {
+      echo "<script>alert('Something Went Wrong. Please try again');</script>";
+    }
+}
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="icon" type="image/x-icon" href="/image/Drivex.png">
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="description" content="DriveX">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:400,700">
+<title>Team Members</title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="../styles/about_responsive.css">
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+<style>
+body {
+	color: #fff;
+	background: #63738a;
+	font-family: 'Roboto', sans-serif;
+}
+.form-control {
+	height: 40px;
+	box-shadow: none;
+	color: #969fa4;
+}
+.form-control:focus {
+	border-color: #5cb85c;
+}
+.form-control, .btn {        
+	border-radius: 3px;
+}
+.signup-form {
+	width: 450px;
+	margin: 0 auto;
+	padding: 30px 0;
+  	font-size: 15px;
+}
+.signup-form h2 {
+	color: #636363;
+	margin: 0 0 15px;
+	position: relative;
+	text-align: center;
+}
+.signup-form h2:before, .signup-form h2:after {
+	content: "";
+	height: 2px;
+	width: 30%;
+	background: #d4d4d4;
+	position: absolute;
+	top: 50%;
+	z-index: 2;
+}	
+.signup-form h2:before {
+	left: 0;
+}
+.signup-form h2:after {
+	right: 0;
+}
+.signup-form .hint-text {
+	color: #999;
+	margin-bottom: 30px;
+	text-align: center;
+}
+.signup-form form {
+	color: #999;
+	border-radius: 3px;
+	margin-bottom: 15px;
+	background: #f2f3f7;
+	box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
+	padding: 30px;
+}
+.signup-form .form-group {
+	margin-bottom: 20px;
+}
+.signup-form input[type="checkbox"] {
+	margin-top: 3px;
+}
+.signup-form .btn {        
+	font-size: 16px;
+	font-weight: bold;		
+	min-width: 140px;
+	outline: none !important;
+}
+.signup-form .row div:first-child {
+	padding-right: 10px;
+}
+.signup-form .row div:last-child {
+	padding-left: 10px;
+}    	
+.signup-form a {
+	color: #fff;
+	text-decoration: underline;
+}
+.signup-form a:hover {
+	text-decoration: none;
+}
+.signup-form form a {
+	color: #5cb85c;
+	text-decoration: none;
+}	
+.signup-form form a:hover {
+	text-decoration: underline;
+}  
+</style>
+</head>
+<body>
+<div class="signup-form">
+    <form  method="POST" enctype="multipart/form-data">
+ <?php
+$eid=$_GET['userid'];
+$ret=mysqli_query($con,"select * from team where id='$eid'");
+while ($row=mysqli_fetch_array($ret)) {
+?>
+		<h2>Update </h2>
+		<p class="hint-text">Update your profile pic.</p>
+<input type="hidden" name="oldpic" value="<?php  echo $row['ProfilePic'];?>">
+	<div class="form-group">
+<img src="profilepics/<?php  echo $row['ProfilePic'];?>" width="120" height="120">
+		</div>
 
+         <div class="form-group">
+        	<input type="file" class="form-control" name="profilepic"  required="true">
+        	<span style="color:red; font-size:12px;">Only jpg / jpeg/ png /gif format allowed.</span>
+        </div> 
+
+
+
+		<div class="form-group">
+            <button type="submit" class="btn btn-success btn-lg btn-block" name="submit">Update</button>
+        </div>
+              <?php 
+}?>
+    </form>
+
+</div>
+</body>
+</html>
