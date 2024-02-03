d4e3accb45eb16c0d93eda32ae468f320d586bbe
Avish Madushanka <avishmadushanka21@gmail.com>
2024-02-03 21:59:53 +0530

    Update Lucid.php

--- Electric/Lucid.php
+++ Electric/Lucid.php
@@ -1,1 +1,178 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title> LUCID Electric cars</title>
+<link rel="icon" type="image/x-icon" href="/image/Drivex.png">
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="description" content="DriveX">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="../styles/bootstrap4/bootstrap.min.css">
+<link href="../plugins/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" type="text/css" href="../plugins/OwlCarousel2-2.2.1/owl.carousel.css">
+<link rel="stylesheet" type="text/css" href="../plugins/OwlCarousel2-2.2.1/owl.theme.default.css">
+<link rel="stylesheet" type="text/css" href="../plugins/OwlCarousel2-2.2.1/animate.css">
+<link rel="stylesheet" type="text/css" href="../styles/classiccars.css">
+<link rel="stylesheet" type="text/css" href="../styles/about_responsive.css">
+</head>
 
+<body>
+
+<div class="super_container">
+	
+	<!-- Header -->
+
+	<header class="header">
+<?php include "header.php"?>
+
+</header>
+
+	<!-- Intro -->
+
+	<div class="intro">
+		<div class="container">
+		
+				</div>
+				
+					<div class="intro_content">
+						<div class="intro_title"><center>LUCID Electrics </center></div>
+						<center class="headline">M&nbspa&nbspd&nbspe&nbsp&nbspi&nbspn&nbsp&nbspA&nbspr&nbspi&nbspz&nbspo&nbspn&nbspa&nbsp</center>
+						<div><div></div><span></span><span></span><span></span></a></div>
+					</div>
+				</div>
+				
+
+			</div>
+		</div>
+	</div>
+<!-- Start blog Area -->
+<section class="blog-area section-gap" id="blog">
+	<div class="container">
+		<div class="row">
+				<div class="col-lg-7 col-md-5 single-blog">
+				<img class="img-fluid" src="../image/lucidair.jpg">
+				<ul class="post-tags">
+					<li>$37,495⁠</li>
+					<li>2020</li>
+					
+				</ul>
+				<h4 class="headline">LUCID AIR</h4>
+				<p>
+					<table>
+						
+						<tr>
+							<td class="topic">Motor : </td>
+							<td td class="descrip">&nbsp1080hp (two motor Combined) </td>
+						</tr>
+
+						<tr>
+							<td class="topic">Power train Capacity : </td>
+							<td td class="descrip">&nbsp805kW</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Battery:  </td>
+							<td class="descrip">&nbsp118kWh</td>
+						</tr>
+						<tr>
+							<td class="topic">Transmission :  </td>
+							<td class="descrip">&nbspmodular single-speed fixed</td>
+						</tr>
+					
+						<tr>
+							<td class="topic">Top Speed :  </td>
+							<td class="descrip">&nbsp378km/h</td>
+						</tr>
+						<tr>
+							<td class="topic">body type :  </td>
+							<td class="descrip">&nbsp4-door sedan </td>
+						</tr>
+						<tr>
+							<td class="topic">Weight :  </td>
+							<td class="descrip">&nbsp 2360kg</td>
+						</tr>
+						<tr>
+							<td class="topic">range :  </td>
+							<td class="descrip">&nbsp840km (EPA est.)</td>
+						</tr>
+					</table>
+				</p>
+				<br><br>
+			</div>
+
+<div class="col-lg-5 col-md-5 single-blog">
+				<img class="img-fluid" src="../image/ucidairsp.jfif">
+				<ul class="post-tags">
+					<li>$325,495⁠</li>
+					<li>2021</li>
+					
+				</ul>
+				<h4 class="headline">LUCID Air Sapphire</h4>
+				<p>
+					<table>
+						
+						<tr>
+							<td class="topic">Motor : </td>
+							<td td class="descrip">&nbsp1200hp (two motor Combined) </td>
+						</tr>
+
+						<tr>
+							<td class="topic">Power train Capacity : </td>
+							<td td class="descrip">&nbsp358kW</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Battery:  </td>
+							<td class="descrip">&nbsp118kWh</td>
+						</tr>
+						<tr>
+							<td class="topic">Transmission :  </td>
+							<td class="descrip">&nbspmodular single-speed fixed</td>
+						</tr>
+					
+						<tr>
+							<td class="topic">Top Speed :  </td>
+							<td class="descrip">&nbsp330km/h</td>
+						</tr>
+						<tr>
+							<td class="topic">body type :  </td>
+							<td class="descrip">&nbsp4-door sedan </td>
+						</tr>
+						<tr>
+							<td class="topic">Weight :  </td>
+							<td class="descrip">&nbsp 2375kg</td>
+						</tr>
+						<tr>
+							<td class="topic">range :  </td>
+							<td class="descrip">&nbsp835km (EPA est.)</td>
+						</tr>
+					</table>
+				</p>
+				<br><br>
+			</div>								
+					
+		
+	</div>	
+</section>
+<!-- End blog Area -->
+<!-- Footer -->
+	
+<?php include "footer.php" ?>
+
+
+<script src="../js/jquery-3.2.1.min.js"></script>
+<script src="../styles/bootstrap4/popper.js"></script>
+<script src="../styles/bootstrap4/bootstrap.min.js"></script>
+<script src="../plugins/greensock/TweenMax.min.js"></script>
+<script src="../plugins/greensock/TimelineMax.min.js"></script>
+<script src="../plugins/scrollmagic/ScrollMagic.min.js"></script>
+<script src="../plugins/greensock/animation.gsap.min.js"></script>
+<script src="../plugins/greensock/ScrollToPlugin.min.js"></script>
+<script src="../plugins/OwlCarousel2-2.2.1/owl.carousel.js"></script>
+<script src="../plugins/easing/easing.js"></script>
+<script src="../plugins/parallax-js-master/parallax.min.js"></script>
+<script src="../js/about_custom.js"></script>
+
+</body>
+
+</html>
