1c072dcec325fbc3e4baa5aed0ec3e521fda6e0c
Avish Madushanka <avishmadushanka21@gmail.com>
2024-02-03 22:02:47 +0530

    Create nio.php

---
+++ Electric/nio.php
@@ -0,0 +1,326 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title> Nio Electric cars</title>
+<link rel="icon" type="image/x-icon" href="/image/Drivex.png">
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="description" content="DriveX">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="../styles/bootstrap4/bootstrap.min.css">
+<link href="../plugins/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" type="text/css" href="../plugins/OwlCarousel2-2.2.1/owl.carousel.css">
+<link rel="stylesheet" type="text/css" href="../plugins/OwlCarousel2-2.2.1/owl.theme.default.css">
+<link rel="stylesheet" type="text/css" href="../plugins/OwlCarousel2-2.2.1/animate.css">
+<link rel="stylesheet" type="text/css" href="../styles/classiccars.css">
+<link rel="stylesheet" type="text/css" href="../styles/about_responsive.css">
+</head>
+
+<body>
+
+<div class="super_container">
+	
+	<!-- Header -->
+
+	
+	<header class="header">
+<?php include "header.php"?>
+
+</header>
+
+	<!-- Intro -->
+
+	<div class="intro">
+		<div class="container">
+		
+				</div>
+				
+					<div class="intro_content">
+						<div class="intro_title"><center>Nio Electric Cars</center></div>
+						<center class="headline">M&nbspa&nbspd&nbspe&nbsp&nbsp i&nbspn&nbsp&nbsp C&nbsph&nbspi&nbspn&nbspa&nbsp</center>
+						<div><div></div><span></span><span></span><span></span></a></div>
+					</div>
+				</div>
+				
+
+			</div>
+		</div>
+	</div>
+<!-- Start blog Area -->
+<section class="blog-area section-gap" id="blog">
+	<div class="container">
+		<div class="row">
+				<div class="col-lg-7 col-md-5 single-blog">
+				<img class="img-fluid" src="https://www.electrifying.com/files/NoQC5VL2bWBrY811/NIO%20ET7%20Test%20Drive%20Event_06%20Oct%202022_34.jpg?width=1500&height=1000">
+				<ul class="post-tags">
+					<li>$69,200⁠</li>
+					<li>2021</li>
+					
+				</ul>
+				<h4 class="headline">ET7</h4>
+				<p>
+					<table>
+						
+						<tr>
+							<td class="topic">Motor : </td>
+							<td td class="descrip">&nbspPerment magnet motor </td>
+						</tr>
+
+						<tr>
+							<td class="topic">Power train Capacity : </td>
+							<td td class="descrip">&nbsp150kWh</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Battery:  </td>
+							<td class="descrip">&nbsp150kwh</td>
+						</tr>
+						<tr>
+							<td class="topic">Transmission :  </td>
+							<td class="descrip">&nbspmodular single-speed gear</td>
+						</tr>
+					
+						<tr>
+							<td class="topic">Top Speed :  </td>
+							<td class="descrip">&nbsp120mph</td>
+						</tr>
+						<tr>
+							<td class="topic">body type :  </td>
+							<td class="descrip">&nbspfour-door sedan</td>
+						</tr>
+						<tr>
+							<td class="topic">range :  </td>
+							<td class="descrip">&nbsp1000km(EPA est.)</td>
+						</tr>
+					</table>
+				</p>
+				<br><br>
+			</div>					
+			<div class="col-lg-5 col-md-5 single-blog">
+				<img class="img-fluid" src="https://carnewschina.com/wp-content/uploads/2022/06/Slide-16_9-1-2022-06-08T203648.544.png">
+				<ul class="post-tags">
+					<li>$46,100</li>
+					<li>2021</li>
+				</ul>
+				<h4 class="headline">ET5</h4>
+				<p>
+                    <table>
+						
+						<tr>
+							<td class="topic">Motor : </td>
+							<td td class="descrip">&nbspPerment magnet motor</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Power train Capacity: </td>
+							<td td class="descrip">&nbsp 155kW</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Battery:  </td>
+							<td class="descrip">&nbsp75kWh</td>
+						</tr>
+						
+						<tr>
+							<td class="topic">Top Speed :  </td>
+							<td class="descrip">&nbsp200km/h</td>
+						</tr>
+						<tr>
+							<td class="topic">body type :  </td>
+							<td class="descrip">&nbsp4 Door sedan </td>
+						</tr>
+						<tr>
+							<td class="topic">range :  </td>
+							<td class="descrip">&nbsp395km (EPA est.)</td>
+						</tr>
+					</table>
+				</p>
+				</p>
+				<br><br>
+			</div>	
+			<div class="col-lg-7 col-md-5 single-blog">
+				<img class="img-fluid" src="https://editorial.pxcrush.net/carsales/general/editorial/2022-nio-es7-0685.jpg?width=496&height=331">
+				<ul class="post-tags">
+					<li>$69,700</li>
+					<li>2022</li>
+				</ul>
+				<h4 class="headline">ES7</h4>
+				<p>
+					<table>
+						
+						<tr>
+							<td class="topic">Motor : </td>
+							<td td class="descrip">&nbsp nbspPerment magnet motor</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Power train Capacity : </td>
+							<td td class="descrip">&nbsp145KW</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Battery:  </td>
+							<td class="descrip">&nbsp75kWh</td>
+						</tr>
+						<tr>
+							<td class="topic">Transmission :  </td>
+							<td class="descrip">&nbspDual speed</td>
+						</tr>
+					
+						<tr>
+							<td class="topic">Top Speed :  </td>
+							<td class="descrip">&nbsp170km/h</td>
+						</tr>
+						<tr>
+							<td class="topic">body type :  </td>
+							<td class="descrip">&nbspSUV five seats </td>
+						</tr>
+						<tr>
+							<td class="topic">range :  </td>
+							<td class="descrip">&nbsp485km (EPA est.)</td>
+						</tr>
+					</table>
+				</p>
+				<br><br>
+			</div>		
+			<div class="col-lg-5 col-md-5 single-blog"> 
+				<img class="img-fluid" src="https://st.arenaev.com/specs/nio/nio-es6-front2.jpg">
+				<ul class="post-tags">
+					<li>$55000</li>
+					<li>2018</li>
+				
+				</ul>
+				<h4 class="headline">ES6</h4>
+				<p>
+					<table>
+						
+						<tr>
+							<td class="topic">Motor : </td>
+							<td td class="descrip">&nbspPermenet magnet motor</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Power train Capacity : </td>
+							<td td class="descrip">&nbsp400kW</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Battery:  </td>
+							<td class="descrip">&nbsp100kWh</td>
+						</tr>
+						<tr>
+							<td class="topic">Transmission :  </td>
+							<td class="descrip">&nbspSingle speed direct drive</td>
+						</tr>
+					
+						<tr>
+							<td class="topic">Top Speed :  </td>
+							<td class="descrip">&nbsp200km/h</td>
+						</tr>
+						<tr>
+							<td class="topic">body type :  </td>
+							<td class="descrip">&nbspSUV fice seated</td>
+						</tr>
+				
+						<tr>
+							<td class="topic">range :  </td>
+							<td class="descrip">&nbsp610km</td>
+						</tr>
+					</table>
+				</p>
+                <br><br>
+				</div>	
+                <div class="col-lg-5 col-md-5 single-blog"> 
+                    <img class="img-fluid" src="https://motortrivia.com/wp-content/uploads/2019/12/04-Nio-EC6-2020.jpg">
+                    <ul class="post-tags">
+                        <li>$49,990</li>
+                        <li>2020</li>
+                    
+                    </ul>
+                    <h4 class="headline">EC6</h4>
+                    <p>
+                        <table>
+                            
+                            <tr>
+                                <td class="topic">Motor : </td>
+                                <td td class="descrip">&nbspperment magnet motor</td>
+                            </tr>
+    
+                            <tr>
+                                <td class="topic">Power train Capacity : </td>
+                                <td td class="descrip">&nbsp400kW(Dual motor Compbine) </td>
+                            </tr>
+    
+                            <tr>
+                                <td class="topic">Battery:  </td>
+                                <td class="descrip">&nbsp70KWh</td>
+                            </tr>
+                            <tr>
+                                <td class="topic">Transmission :  </td>
+                                <td class="descrip">&nbspSingle speed direct drive</td>
+                            </tr>
+                        
+                            <tr>
+                                <td class="topic">Top Speed :  </td>
+                                <td class="descrip">&nbsp200km/h</td>
+                            </tr>
+                            <tr>
+                                <td class="topic">body type :  </td>
+                                <td class="descrip">&nbspCrossover Coupe five seates</td>
+                            </tr>
+                    
+                            <tr>
+                                <td class="topic">range :  </td>
+                                <td class="descrip">&nbsp615km</td>
+                            </tr>
+                        </table>
+                    </p>
+                    <br><br>
+                    </div>	
+                
+			
+		
+	</div>	
+</section>
+<!-- End blog Area -->
+<!-- Footer -->
+	
+<footer>
+    <div class="footer-top text-center">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-6 text-center">
+                    <h4 class="navbar-brand">DRIVEX</h4>
+                    <p>We provide information about all types of cars in the world to all car addictors. We provide information about classic cars, luxury cars, sports cars and new arrivals. Also we have a range of articles about cars as well as our social media pages provide you additional insights about all types of cars.</p>
+                    <div class="col-auto social-icons">
+                        <a href="#"><i class='fa fa-facebook-f'></i></a>
+                        <a href="#"><i class='fa fa-twitter'></i></a>
+                        <a href="#"><i class='fa fa-instagram'></i></a>
+                        
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer-bottom text-center">
+        <p class="mb-0"></p> Made With <i class="fa fa-heart-o" aria-hidden="true"></i> By ICONS
+            
+    </div>
+</footer>
+
+
+<script src="../js/jquery-3.2.1.min.js"></script>
+<script src="../styles/bootstrap4/popper.js"></script>
+<script src="../styles/bootstrap4/bootstrap.min.js"></script>
+<script src="../plugins/greensock/TweenMax.min.js"></script>
+<script src="../plugins/greensock/TimelineMax.min.js"></script>
+<script src="../plugins/scrollmagic/ScrollMagic.min.js"></script>
+<script src="../plugins/greensock/animation.gsap.min.js"></script>
+<script src="../plugins/greensock/ScrollToPlugin.min.js"></script>
+<script src="../plugins/OwlCarousel2-2.2.1/owl.carousel.js"></script>
+<script src="../plugins/easing/easing.js"></script>
+<script src="../plugins/parallax-js-master/parallax.min.js"></script>
+<script src="../js/about_custom.js"></script>
+
+</body>
