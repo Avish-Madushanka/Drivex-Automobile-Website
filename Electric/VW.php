748dee74f9e87d1e3186060cad62e66e8c097cb2
Avish Madushanka <avishmadushanka21@gmail.com>
2024-02-03 22:00:34 +0530

    Create VW.php

---
+++ Electric/VW.php
@@ -0,0 +1,408 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title> Volkswagen Electric cars</title>
+<link rel="icon" type="image/x-icon" href="/image/Drivex.png">
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="description" content="DriveX">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="../styles/bootstrap4/bootstrap.min.css">
+<link href="../plugins/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" type="text/css" href="../plugins/OwlCarousel2-2.2.1/owl.carousel.css">
+<link rel="stylesheet" type="text/css" href="../plugins/OwlCarousel2-2.2.1/owl.theme.default.css">
+<link rel="stylesheet" type="text/css" href="../plugins/OwlCarousel2-2.2.1/animate.css">
+<link rel="stylesheet" type="text/css" href="../styles/classiccars.css">
+<link rel="stylesheet" type="text/css" href="../styles/about_responsive.css">
+</head>
+
+<body>
+
+<div class="super_container">
+	
+	<!-- Header -->
+
+
+	<header class="header">
+<?php include "header.php"?>
+
+</header>
+	
+
+	<!-- Intro -->
+
+	<div class="intro">
+		<div class="container">
+		
+				</div>
+				
+					<div class="intro_content">
+						<div class="intro_title"><center>Volkswagen Electric cars </center></div>
+						<center class="headline">M&nbspa&nbspd&nbspe&nbsp&nbsp i&nbspn&nbsp&nbsp G&nbspe&nbspr&nbspm&nbspa&nbspn&nbspy</center>
+						<div><div></div><span></span><span></span><span></span></a></div>
+					</div>
+				</div>
+				
+
+			</div>
+		</div>
+	</div>
+<!-- Start blog Area -->
+<section class="blog-area section-gap" id="blog">
+	<div class="container">
+		<div class="row">
+				<div class="col-lg-7 col-md-5 single-blog">
+				<img class="img-fluid" src="https://www.vw.com/api/assets/onehub_pkw/us/en/gallery/id4/2023/exterior/VW_NGW6_Showroom_ID4_Gallery_Exterior-1_ph2.jpg/renditions/original">
+				<ul class="post-tags">
+					<li>$37,495⁠</li>
+					<li>2020</li>
+					
+				</ul>
+				<h4 class="headline">VW ID.4</h4>
+				<p>
+					<table>
+						
+						<tr>
+							<td class="topic">Motor : </td>
+							<td td class="descrip">&nbspPerment magnet brushless </td>
+						</tr>
+
+						<tr>
+							<td class="topic">Power train Capacity : </td>
+							<td td class="descrip">&nbsp109-225kW</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Battery:  </td>
+							<td class="descrip">&nbsp62KWh</td>
+						</tr>
+						<tr>
+							<td class="topic">Transmission :  </td>
+							<td class="descrip">&nbspmodular single-speed gear</td>
+						</tr>
+					
+						<tr>
+							<td class="topic">Top Speed :  </td>
+							<td class="descrip">&nbsp160km/h</td>
+						</tr>
+						<tr>
+							<td class="topic">body type :  </td>
+							<td class="descrip">&nbspCross over  </td>
+						</tr>
+						<tr>
+							<td class="topic">Peak power :  </td>
+							<td class="descrip">&nbsp127kW-128kW</td>
+						</tr>
+						
+						<tr>
+							<td class="topic">Weight :  </td>
+							<td class="descrip">&nbsp 2124kg</td>
+						</tr>
+						<tr>
+							<td class="topic">range :  </td>
+							<td class="descrip">&nbsp410km (EPA est.)</td>
+						</tr>
+					</table>
+				</p>
+				<br><br>
+			</div>					
+			<div class="col-lg-5 col-md-5 single-blog">
+				<img class="img-fluid" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/2020_Volkswagen_ID.3_1st_Front.jpg/420px-2020_Volkswagen_ID.3_1st_Front.jpg">
+				<ul class="post-tags">
+					<li>$105,000</li>
+					<li>2020</li>
+				</ul>
+				<h4 class="headline">VW ID.3</h4>
+				<p>
+                    <table>
+						
+						<tr>
+							<td class="topic">Motor : </td>
+							<td td class="descrip">&nbsp 825bhp (AC induction Motor)</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Power train Capacity: </td>
+							<td td class="descrip">&nbsp 615kW (Dual motor Combine)</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Battery:  </td>
+							<td class="descrip">&nbsp100kWh Li-ion</td>
+						</tr>
+						<tr>
+							<td class="topic">Transmission :  </td>
+							<td class="descrip">&nbspsingle speed transaxle</td>
+						</tr>
+					
+						<tr>
+							<td class="topic">Top Speed :  </td>
+							<td class="descrip">&nbsp155mph</td>
+						</tr>
+						<tr>
+							<td class="topic">body type :  </td>
+							<td class="descrip">&nbsp4 Door SUV with two falcon doors (7 seated) </td>
+						</tr>
+						<tr>
+							<td class="topic">Peak power :  </td>
+							<td class="descrip">&nbsp 670hp</td>
+						</tr>
+						
+						<tr>
+							<td class="topic">Weight :  </td>
+							<td class="descrip">&nbsp 5,185 lbs</td>
+						</tr>
+						<tr>
+							<td class="topic">range :  </td>
+							<td class="descrip">&nbsp348miles (EPA est.)</td>
+						</tr>
+					</table>
+				</p>
+				</p>
+				<br><br>
+			</div>	
+			<div class="col-lg-7 col-md-5 single-blog">
+				<img class="img-fluid" src="https://images.carexpert.com.au/crop/372/248/app/uploads/2020/10/volkswagen-id-electric-2.jpg">
+				<ul class="post-tags">
+					<li>$16,010</li>
+					<li>2019</li>
+				</ul>
+				<h4 class="headline">VW ID.2</h4>
+				<p>
+					<table>
+						
+						<tr>
+							<td class="topic">Motor : </td>
+							<td td class="descrip">&nbsp  IMP-SynRM</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Power train Capacity : </td>
+							<td td class="descrip">&nbsp 615kW (Dual motor Combine)</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Battery:  </td>
+							<td class="descrip">&nbsp82kWh Li-ion</td>
+						</tr>
+						<tr>
+							<td class="topic">Transmission :  </td>
+							<td class="descrip">&nbspAutomatic</td>
+						</tr>
+					
+						<tr>
+							<td class="topic">Top Speed :  </td>
+							<td class="descrip">&nbsp135mph</td>
+						</tr>
+						<tr>
+							<td class="topic">body type :  </td>
+							<td class="descrip">&nbsp4 Door Cross over (7 seated) </td>
+						</tr>
+						<tr>
+							<td class="topic">Peak power :  </td>
+							<td class="descrip">&nbsp 670hp</td>
+						</tr>
+						
+						<tr>
+							<td class="topic">Weight :  </td>
+							<td class="descrip">&nbsp 4,363 lbs</td>
+						</tr>
+						<tr>
+							<td class="topic">range :  </td>
+							<td class="descrip">&nbsp330 miles (EPA est.)</td>
+						</tr>
+					</table>
+				</p>
+				<br><br>
+			</div>		
+			<div class="col-lg-5 col-md-5 single-blog"> 
+				<img class="img-fluid" src="https://cdn.motor1.com/images/mgl/9L3Ly/s3/vw-id.5-sedan.webp">
+				<ul class="post-tags">
+					<li>$49,990</li>
+					<li>2017</li>
+					<li>United States</li>
+				</ul>
+				<h4 class="headline">VW ID.5</h4>
+				<p>
+					<table>
+						
+						<tr>
+							<td class="topic">Motor : </td>
+							<td td class="descrip">&nbsp IMP-SynRM</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Power train Capacity : </td>
+							<td td class="descrip">&nbsp 165kW </td>
+						</tr>
+
+						<tr>
+							<td class="topic">Battery:  </td>
+							<td class="descrip">&nbsp50kWh Li-ion</td>
+						</tr>
+						<tr>
+							<td class="topic">Transmission :  </td>
+							<td class="descrip">&nbspSingle speed fixed gear</td>
+						</tr>
+					
+						<tr>
+							<td class="topic">Top Speed :  </td>
+							<td class="descrip">&nbsp140mph</td>
+						</tr>
+						<tr>
+							<td class="topic">body type :  </td>
+							<td class="descrip">&nbspCompact executive sedan (5 seated) </td>
+						</tr>
+						<tr>
+							<td class="topic">Peak power :  </td>
+							<td class="descrip">&nbsp 221hp</td>
+						</tr>
+						
+						<tr>
+							<td class="topic">Weight :  </td>
+							<td class="descrip">&nbsp 4,034lbs</td>
+						</tr>
+						<tr>
+							<td class="topic">range :  </td>
+							<td class="descrip">&nbsp358miles (EPA est.)</td>
+						</tr>
+					</table>
+				</p>
+                <br><br>
+				</div>	
+                
+			<div class="col-lg-7 col-md-5 single-blog">
+				<img class="img-fluid" src="../image/vwid.6.jfif">
+				<ul class="post-tags">
+					<li>$94,363</li>
+					<li>1966</li>
+					<li>Japan</li>
+				</ul>
+				<h4 class="headline">VW ID.6</h4>
+				<p>
+					<table>
+						
+						<tr>
+							<td class="topic">Motor : </td>
+							<td td class="descrip">&nbsp 248hp (AC induction Motor)</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Power train Capacity : </td>
+							<td td class="descrip">&nbsp 913kW </td>
+						</tr>
+
+						<tr>
+							<td class="topic">Battery:  </td>
+							<td class="descrip">&nbsp200kWh Li-ion</td>
+						</tr>
+						<tr>
+							<td class="topic">Transmission :  </td>
+							<td class="descrip">&nbspsingle speed gear</td>
+						</tr>
+					
+						<tr>
+							<td class="topic">Top Speed :  </td>
+							<td class="descrip">&nbsp249mph</td>
+						</tr>
+						<tr>
+							<td class="topic">body type :  </td>
+							<td class="descrip">&nbspRoadster ( 4 seated) </td>
+						</tr>
+						<tr>
+							<td class="topic">Peak power :  </td>
+							<td class="descrip">&nbsp 1245hphp</td>
+						</tr>
+						
+						<tr>
+							<td class="topic">Weight :  </td>
+							<td class="descrip">&nbsp 4,409lbs</td>
+						</tr>
+						<tr>
+							<td class="topic">range :  </td>
+							<td class="descrip">&nbsp600miles (EPA est.)</td>
+						</tr>
+					</table>
+				</p>
+				<br><br>
+			</div>
+            <div class="col-lg-5 col-md-5 single-blog"> 
+				<img class="img-fluid" src="https://www.motortrend.com/uploads/sites/5/2020/07/2018-Tesla-Model-3-18.jpg?fit=around%7C875:492">
+				<ul class="post-tags">
+					<li>$49,990</li>
+					<li>2017</li>
+					<li>United States</li>
+				</ul>
+				<h4 class="headline">VW ID.7</h4>
+				<p>
+					<table>
+						
+						<tr>
+							<td class="topic">Motor : </td>
+							<td td class="descrip">&nbsp IMP-SynRM</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Power train Capacity : </td>
+							<td td class="descrip">&nbsp 165kW </td>
+						</tr>
+
+						<tr>
+							<td class="topic">Battery:  </td>
+							<td class="descrip">&nbsp50kWh Li-ion</td>
+						</tr>
+						<tr>
+							<td class="topic">Transmission :  </td>
+							<td class="descrip">&nbspSingle speed fixed gear</td>
+						</tr>
+					
+						<tr>
+							<td class="topic">Top Speed :  </td>
+							<td class="descrip">&nbsp140mph</td>
+						</tr>
+						<tr>
+							<td class="topic">body type :  </td>
+							<td class="descrip">&nbspCompact executive sedan (5 seated) </td>
+						</tr>
+						<tr>
+							<td class="topic">Peak power :  </td>
+							<td class="descrip">&nbsp 221hp</td>
+						</tr>
+						
+						<tr>
+							<td class="topic">Weight :  </td>
+							<td class="descrip">&nbsp 4,034lbs</td>
+						</tr>
+						<tr>
+							<td class="topic">range :  </td>
+							<td class="descrip">&nbsp358miles (EPA est.)</td>
+						</tr>
+					</table>
+				</p>
+                <br><br>
+				</div>		
+		
+	</div>	
+</section>
+<!-- End blog Area -->
+<!-- Footer -->
+	
+<?php include "footer.php" ?>
+
+
+<script src="../js/jquery-3.2.1.min.js"></script>
+<script src="../styles/bootstrap4/popper.js"></script>
+<script src="../styles/bootstrap4/bootstrap.min.js"></script>
+<script src="../plugins/greensock/TweenMax.min.js"></script>
+<script src="../plugins/greensock/TimelineMax.min.js"></script>
+<script src="../plugins/scrollmagic/ScrollMagic.min.js"></script>
+<script src="../plugins/greensock/animation.gsap.min.js"></script>
+<script src="../plugins/greensock/ScrollToPlugin.min.js"></script>
+<script src="../plugins/OwlCarousel2-2.2.1/owl.carousel.js"></script>
+<script src="../plugins/easing/easing.js"></script>
+<script src="../plugins/parallax-js-master/parallax.min.js"></script>
+<script src="../js/about_custom.js"></script>
+
+</body>
+
+</html>
