25c1e0e40af3eac0f9d4321c7ee36c1c48c326f5
Avish Madushanka <avishmadushanka21@gmail.com>
2024-02-03 22:00:51 +0530

    Create benz.php

---
+++ Electric/benz.php
@@ -0,0 +1,282 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title> Benz Electric cars</title>
+<link rel="icon" type="image/x-icon" href="/image/Drivex.png">
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="description" content="DriveX">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="../styles/bootstrap4/bootstrap.min.css">
+<link href="../plugins/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" type="text/css" href="../plugins/OwlCarousel2-2.2.1/owl.carousel.css">
+<link rel="stylesheet" type="text/css" href="../plugins/OwlCarousel2-2.2.1/owl.theme.default.css">
+<link rel="stylesheet" type="text/css" href="../plugins/OwlCarousel2-2.2.1/animate.css">
+<link rel="stylesheet" type="text/css" href="../styles/classiccars.css">
+<link rel="stylesheet" type="text/css" href="../styles/about_responsive.css">
+</head>
+
+<body>
+
+<div class="super_container">
+	
+	<!-- Header -->
+
+	<header class="header">
+<?php include "header.php"?>
+
+</header>
+
+	<!-- Intro -->
+
+	<div class="intro">
+		<div class="container">
+		
+				</div>
+				
+					<div class="intro_content">
+						<div class="intro_title"><center>Benz Electrics </center></div>
+						<center class="headline">M&nbspa&nbspd&nbspe&nbsp&nbsp i&nbspn&nbsp&nbsp G&nbspe&nbspr&nbspm&nbspa&nbspn&nbspy</center>
+						<div><div></div><span></span><span></span><span></span></a></div>
+					</div>
+				</div>
+				
+
+			</div>
+		</div>
+	</div>
+<!-- Start blog Area -->
+<section class="blog-area section-gap" id="blog">
+	<div class="container">
+		<div class="row">
+				<div class="col-lg-7 col-md-5 single-blog">
+				<img class="img-fluid" src="../image/benzeqs580.jpg">
+				<ul class="post-tags">
+					<li>$160,597⁠</li>
+					<li>2022</li>
+					
+				</ul>
+				<h4 class="headline">EQS Benz580</h4>
+				<p>
+					<table>
+						
+						<tr>
+							<td class="topic">Motor : </td>
+							<td td class="descrip">&nbsp329hp</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Power train Capacity : </td>
+							<td td class="descrip">&nbsp120kW</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Battery:  </td>
+							<td class="descrip">&nbsp107kWh</td>
+						</tr>
+						<tr>
+							<td class="topic">Transmission :  </td>
+							<td class="descrip">&nbspmodular single-speed fixed</td>
+						</tr>
+					
+						<tr>
+							<td class="topic">Top Speed :  </td>
+							<td class="descrip">&nbsp210km/h</td>
+						</tr>
+						<tr>
+							<td class="topic">body type :  </td>
+							<td class="descrip">&nbsp4-doors </td>
+						</tr>
+						<tr>
+							<td class="topic">Weight :  </td>
+							<td class="descrip">&nbsp 2585kg</td>
+						</tr>
+						<tr>
+							<td class="topic">range :  </td>
+							<td class="descrip">&nbsp450-850km</td>
+						</tr>
+					</table>
+				</p>
+				<br><br>
+			</div>
+
+<div class="col-lg-5 col-md-5 single-blog">
+				<img class="img-fluid" src="../image/eqs450.jpg">
+				<ul class="post-tags">
+					<li>$102,310⁠</li>
+					<li>2022</li>
+					
+				</ul>
+				<h4 class="headline">EQS 450+</h4>
+				<p>
+					<table>
+						
+						<tr>
+							<td class="topic">Motor : </td>
+							<td td class="descrip">&nbsp329hp </td>
+						</tr>
+
+						<tr>
+							<td class="topic">Power train Capacity : </td>
+							<td td class="descrip">&nbsp200kW</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Battery:  </td>
+							<td class="descrip">&nbsp106kWh</td>
+						</tr>
+						<tr>
+							<td class="topic">Transmission :  </td>
+							<td class="descrip">&nbspmodular single-speed fixed</td>
+						</tr>
+					
+						<tr>
+							<td class="topic">Top Speed :  </td>
+							<td class="descrip">&nbsp210km/h</td>
+						</tr>
+						<tr>
+							<td class="topic">body type :  </td>
+							<td class="descrip">&nbsp4-door sedan </td>
+						</tr>
+						<tr>
+							<td class="topic">Weight :  </td>
+							<td class="descrip">&nbsp 2480kg</td>
+						</tr>
+						<tr>
+							<td class="topic">range :  </td>
+							<td class="descrip">&nbsp780km</td>
+						</tr>
+					</table>
+				</p>
+				<br><br>
+			</div>		
+            <div class="container">
+		<div class="row">
+				<div class="col-lg-7 col-md-5 single-blog">
+				<img class="img-fluid" src="../image/eqxx.jpg">
+				<ul class="post-tags">
+					<li>$75,700⁠</li>
+					<li>2022</li>
+					
+				</ul>
+				<h4 class="headline">Mercedes EQXX</h4>
+				<p>
+					<table>
+						
+						<tr>
+							<td class="topic">Motor : </td>
+							<td td class="descrip">&nbsp756hp</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Power train Capacity : </td>
+							<td td class="descrip">&nbsp100kW</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Battery:  </td>
+							<td class="descrip">&nbsp107kWh</td>
+						</tr>
+						<tr>
+							<td class="topic">Transmission :  </td>
+							<td class="descrip">&nbspmodular single-speed fixed</td>
+						</tr>
+					
+						<tr>
+							<td class="topic">Top Speed :  </td>
+							<td class="descrip">&nbsp140km/h</td>
+						</tr>
+						<tr>
+							<td class="topic">body type :  </td>
+							<td class="descrip">&nbsp4-doors </td>
+						</tr>
+						<tr>
+							<td class="topic">Weight :  </td>
+							<td class="descrip">&nbsp 1755kg</td>
+						</tr>
+						<tr>
+							<td class="topic">range :  </td>
+							<td class="descrip">&nbsp1202km</td>
+						</tr>
+					</table>
+				</p>
+				<br><br>
+			</div>
+
+<div class="col-lg-5 col-md-5 single-blog">
+				<img class="img-fluid" src="../image/eqssclass.jpg">
+				<ul class="post-tags">
+					<li>$102,310⁠</li>
+					<li>2022</li>
+					
+				</ul>
+				<h4 class="headline">Mercedes Electric S Class</h4>
+				<p>
+					<table>
+						
+						<tr>
+							<td class="topic">Motor : </td>
+							<td td class="descrip">&nbsp516hp </td>
+						</tr>
+
+						<tr>
+							<td class="topic">Power train Capacity : </td>
+							<td td class="descrip">&nbsp290kW</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Battery:  </td>
+							<td class="descrip">&nbsp100kWh</td>
+						</tr>
+						<tr>
+							<td class="topic">Transmission :  </td>
+							<td class="descrip">&nbspmodular single-speed fixed</td>
+						</tr>
+					
+						<tr>
+							<td class="topic">Top Speed :  </td>
+							<td class="descrip">&nbsp210km/h</td>
+						</tr>
+						<tr>
+							<td class="topic">body type :  </td>
+							<td class="descrip">&nbsp4-door sedan </td>
+						</tr>
+						<tr>
+							<td class="topic">Weight :  </td>
+							<td class="descrip">&nbsp 2585kg</td>
+						</tr>
+						<tr>
+							<td class="topic">range :  </td>
+							<td class="descrip">&nbsp 545km</td>
+						</tr>
+					</table>
+				</p>
+				<br><br>
+			</div>								
+											
+					
+		
+	</div>	
+</section>
+<!-- End blog Area -->
+<!-- Footer -->
+	
+<?php include "footer.php" ?>
+
+
+<script src="../js/jquery-3.2.1.min.js"></script>
+<script src="../styles/bootstrap4/popper.js"></script>
+<script src="../styles/bootstrap4/bootstrap.min.js"></script>
+<script src="../plugins/greensock/TweenMax.min.js"></script>
+<script src="../plugins/greensock/TimelineMax.min.js"></script>
+<script src="../plugins/scrollmagic/ScrollMagic.min.js"></script>
+<script src="../plugins/greensock/animation.gsap.min.js"></script>
+<script src="../plugins/greensock/ScrollToPlugin.min.js"></script>
+<script src="../plugins/OwlCarousel2-2.2.1/owl.carousel.js"></script>
+<script src="../plugins/easing/easing.js"></script>
+<script src="../plugins/parallax-js-master/parallax.min.js"></script>
+<script src="../js/about_custom.js"></script>
+
+</body>
+
+</html>
