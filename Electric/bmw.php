2782fa54b1ea02665480c576466828e359c647f7
Avish Madushanka <avishmadushanka21@gmail.com>
2024-02-03 22:01:11 +0530

    Create bmw.php

---
+++ Electric/bmw.php
@@ -0,0 +1,282 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title> BMW Electric cars</title>
+<link rel="icon" type="image/x-icon" href="/image/Drivex.png">
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="description" content="DriveX">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="../styles/bootstrap4/bootstrap.min.css">
+<link href="../plugins/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" type="text/css" href="../plugins/OwlCarousel2-2.2.1/owl.carousel.css">
+<link rel="stylesheet" type="text/css" href="../plugins/OwlCarousel2-2.2.1/owl.theme.default.css">
+<link rel="stylesheet" type="text/css" href="../plugins/OwlCarousel2-2.2.1/animate.css">
+<link rel="stylesheet" type="text/css" href="../styles/classiccars.css">
+<link rel="stylesheet" type="text/css" href="../styles/about_responsive.css">
+</head>
+
+<body>
+
+<div class="super_container">
+	
+	<!-- Header -->
+
+	<header class="header">
+<?php include "header.php"?>
+
+</header>
+
+	<!-- Intro -->
+
+	<div class="intro">
+		<div class="container">
+		
+				</div>
+				
+					<div class="intro_content">
+						<div class="intro_title"><center>BMW Electrics </center></div>
+						<center class="headline">M&nbspa&nbspd&nbspe&nbsp&nbsp i&nbspn&nbsp&nbsp G&nbspe&nbspr&nbspm&nbspa&nbspn&nbspy</center>
+						<div><div></div><span></span><span></span><span></span></a></div>
+					</div>
+				</div>
+				
+
+			</div>
+		</div>
+	</div>
+<!-- Start blog Area -->
+<section class="blog-area section-gap" id="blog">
+	<div class="container">
+		<div class="row">
+				<div class="col-lg-7 col-md-5 single-blog">
+				<img class="img-fluid" src="../image/i4e40.jpg">
+				<ul class="post-tags">
+					<li>$56,395⁠</li>
+					<li>2022</li>
+					
+				</ul>
+				<h4 class="headline">I4 EDrive40</h4>
+				<p>
+					<table>
+						
+						<tr>
+							<td class="topic">Motor : </td>
+							<td td class="descrip">&nbsp335hp</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Power train Capacity : </td>
+							<td td class="descrip">&nbsp116kW</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Battery:  </td>
+							<td class="descrip">&nbsp80.7kWh</td>
+						</tr>
+						<tr>
+							<td class="topic">Transmission :  </td>
+							<td class="descrip">&nbspmodular single-speed fixed</td>
+						</tr>
+					
+						<tr>
+							<td class="topic">Top Speed :  </td>
+							<td class="descrip">&nbsp190km/h</td>
+						</tr>
+						<tr>
+							<td class="topic">body type :  </td>
+							<td class="descrip">&nbsp4-doors </td>
+						</tr>
+						<tr>
+							<td class="topic">Weight :  </td>
+							<td class="descrip">&nbsp 2050kg</td>
+						</tr>
+						<tr>
+							<td class="topic">range :  </td>
+							<td class="descrip">&nbsp340-670km</td>
+						</tr>
+					</table>
+				</p>
+				<br><br>
+			</div>
+
+<div class="col-lg-5 col-md-5 single-blog">
+				<img class="img-fluid" src="../image/i4m50.jpg">
+				<ul class="post-tags">
+					<li>$52,395⁠</li>
+					<li>2022</li>
+					
+				</ul>
+				<h4 class="headline">I4 M50</h4>
+				<p>
+					<table>
+						
+						<tr>
+							<td class="topic">Motor : </td>
+							<td td class="descrip">&nbsp1536hp </td>
+						</tr>
+
+						<tr>
+							<td class="topic">Power train Capacity : </td>
+							<td td class="descrip">&nbsp195kW</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Battery:  </td>
+							<td class="descrip">&nbsp83.9kWh</td>
+						</tr>
+						<tr>
+							<td class="topic">Transmission :  </td>
+							<td class="descrip">&nbspmodular single-speed fixed</td>
+						</tr>
+					
+						<tr>
+							<td class="topic">Top Speed :  </td>
+							<td class="descrip">&nbsp227km/h</td>
+						</tr>
+						<tr>
+							<td class="topic">body type :  </td>
+							<td class="descrip">&nbsp4-door sedan </td>
+						</tr>
+						<tr>
+							<td class="topic">Weight :  </td>
+							<td class="descrip">&nbsp 2215kg</td>
+						</tr>
+						<tr>
+							<td class="topic">range :  </td>
+							<td class="descrip">&nbsp510km</td>
+						</tr>
+					</table>
+				</p>
+				<br><br>
+			</div>		
+            <div class="container">
+		<div class="row">
+				<div class="col-lg-7 col-md-5 single-blog">
+				<img class="img-fluid" src="../image/i7x60.jpg">
+				<ul class="post-tags">
+					<li>$119,300⁠</li>
+					<li>2022</li>
+					
+				</ul>
+				<h4 class="headline">I7 XDrive60</h4>
+				<p>
+					<table>
+						
+						<tr>
+							<td class="topic">Motor : </td>
+							<td td class="descrip">&nbsp544hp</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Power train Capacity : </td>
+							<td td class="descrip">&nbsp105kW</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Battery:  </td>
+							<td class="descrip">&nbsp101.7kWh</td>
+						</tr>
+						<tr>
+							<td class="topic">Transmission :  </td>
+							<td class="descrip">&nbspmodular single-speed fixed</td>
+						</tr>
+					
+						<tr>
+							<td class="topic">Top Speed :  </td>
+							<td class="descrip">&nbsp110km/h</td>
+						</tr>
+						<tr>
+							<td class="topic">body type :  </td>
+							<td class="descrip">&nbsp4-doors </td>
+						</tr>
+						<tr>
+							<td class="topic">Weight :  </td>
+							<td class="descrip">&nbsp 2715kg</td>
+						</tr>
+						<tr>
+							<td class="topic">range :  </td>
+							<td class="descrip">&nbsp375-715km</td>
+						</tr>
+					</table>
+				</p>
+				<br><br>
+			</div>
+
+<div class="col-lg-5 col-md-5 single-blog">
+				<img class="img-fluid" src="../image/i8.jpg">
+				<ul class="post-tags">
+					<li>$147,500⁠</li>
+					<li>2022</li>
+					
+				</ul>
+				<h4 class="headline">BMW I8</h4>
+				<p>
+					<table>
+						
+						<tr>
+							<td class="topic">Motor : </td>
+							<td td class="descrip">&nbsp369hp </td>
+						</tr>
+
+						<tr>
+							<td class="topic">Power train Capacity : </td>
+							<td td class="descrip">&nbsp279kW</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Battery:  </td>
+							<td class="descrip">&nbsp11.6kWh</td>
+						</tr>
+						<tr>
+							<td class="topic">Transmission :  </td>
+							<td class="descrip">&nbspmodular single-speed fixed</td>
+						</tr>
+					
+						<tr>
+							<td class="topic">Top Speed :  </td>
+							<td class="descrip">&nbsp250km/h</td>
+						</tr>
+						<tr>
+							<td class="topic">body type :  </td>
+							<td class="descrip">&nbsp2-door sedan </td>
+						</tr>
+						<tr>
+							<td class="topic">Weight :  </td>
+							<td class="descrip">&nbsp 1595kg</td>
+						</tr>
+						<tr>
+							<td class="topic">range :  </td>
+							<td class="descrip">&nbsp440km</td>
+						</tr>
+					</table>
+				</p>
+				<br><br>
+			</div>								
+											
+					
+		
+	</div>	
+</section>
+<!-- End blog Area -->
+<!-- Footer -->
+	
+<?php include "footer.php" ?>
+
+
+<script src="../js/jquery-3.2.1.min.js"></script>
+<script src="../styles/bootstrap4/popper.js"></script>
+<script src="../styles/bootstrap4/bootstrap.min.js"></script>
+<script src="../plugins/greensock/TweenMax.min.js"></script>
+<script src="../plugins/greensock/TimelineMax.min.js"></script>
+<script src="../plugins/scrollmagic/ScrollMagic.min.js"></script>
+<script src="../plugins/greensock/animation.gsap.min.js"></script>
+<script src="../plugins/greensock/ScrollToPlugin.min.js"></script>
+<script src="../plugins/OwlCarousel2-2.2.1/owl.carousel.js"></script>
+<script src="../plugins/easing/easing.js"></script>
+<script src="../plugins/parallax-js-master/parallax.min.js"></script>
+<script src="../js/about_custom.js"></script>
+
+</body>
+
+</html>
