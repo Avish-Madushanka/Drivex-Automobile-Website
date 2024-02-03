523491b31d775b6c1b582b8bb661f0b587f8419b
Avish Madushanka <avishmadushanka21@gmail.com>
2024-02-03 22:01:55 +0530

    Create footer.php

---
+++ Electric/footer.php
@@ -0,0 +1,25 @@
+<html>
+    <footer>
+        <div class="footer-top text-center">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-6 text-center">
+                        <h4 class="navbar-brand">DRIVEX<span class="dot">.</span></h4>
+                        <p>We provide information about all types of cars in the world to all car addictors. We provide information about classic cars, luxury cars, sports cars, electric cars and new arrivals. Also we have a range of articles about cars as well as our social media pages provide you additional insights about all types of cars.</p>
+                        <div class="col-auto social-icons">
+                            <a href="#"><i class='fa fa-facebook-f'></i></a>
+                            <a href="#"><i class='fa fa-twitter'></i></a>
+                            <a href="#"><i class='fa fa-instagram'></i></a>
+                            
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer-bottom text-center">
+            <p class="mb-0"></p> Made With <i class="fa fa-heart-o" aria-hidden="true"></i> By ICONS
+                
+        </div>
+    </footer>
+    </html>
