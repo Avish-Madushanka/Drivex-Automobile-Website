6e16f4cb0ef38989543ea27f1c5ce21609eb2ac0
Avish Madushanka <avishmadushanka21@gmail.com>
2024-02-09 21:12:33 +0530

    Update minicoopclas.php

--- classic/minicoopclas.php
+++ classic/minicoopclas.php
@@ -1,1 +1,313 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title> Mini Cooper Classic Cars</title>
+<link rel="icon" type="image/x-icon" href="/image/Drivex.png">
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="description" content="DriveX">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="../styles/bootstrap4/bootstrap.min.css">
+<link href="../plugins/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" type="text/css" href="../plugins/OwlCarousel2-2.2.1/owl.carousel.css">
+<link rel="stylesheet" type="text/css" href="../plugins/OwlCarousel2-2.2.1/owl.theme.default.css">
+<link rel="stylesheet" type="text/css" href="../plugins/OwlCarousel2-2.2.1/animate.css">
+<link rel="stylesheet" type="text/css" href="../styles/classiccars.css">
+<link rel="stylesheet" type="text/css" href="../styles/about_responsive.css">
+</head>
 
+<body>
+
+<div class="super_container">
+	
+	<!-- Header -->
+
+	<header class="header">
+<?php include "headerclss.php"?>
+
+</header>
+
+
+	<!-- Intro -->
+
+	<div class="intro">
+		<div class="container">
+		
+				</div>
+				
+					<div class="intro_content">
+						<div class="intro_title"><center>Mini Cooper Classic Cars</center></div>
+						
+						<div><div></div><span></span><span></span><span></span></a></div>
+					</div>
+				</div>
+				
+
+			</div>
+		</div>
+	</div>
+<!-- Start blog Area -->
+<section class="blog-area section-gap" id="blog">
+	<div class="container">
+		<div class="row">
+				<div class="col-lg-7 col-md-5 single-blog">
+				<img class="img-fluid" src="../image/clmini1971.jpg">
+				<ul class="post-tags">
+					<li>$16,440</li>
+					<li>1959</li>
+					<li>UK</li>
+					
+				</ul>
+				<h4 class="headline">Morris Mini-Minor </h4>
+				<p>
+					<table>
+						
+						<tr>
+							<td class="topic">Engine : </td>
+							<td td class="descrip">&nbsp&nbsp1.0L</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Engine Capacity : </td>
+							<td td class="descrip">&nbsp&nbsp1275cc</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Fuel Type :  </td>
+							<td class="descrip">&nbsp Petrol (Gasoline)</td>
+						</tr>
+						<tr>
+							<td class="topic">Transmission :  </td>
+							<td class="descrip">&nbsp 4 Speed Manual</td>
+						</tr>
+						<tr>
+							<td class="topic">Fuel Tank Capacity :  </td>
+							<td class="descrip">&nbsp 25l</td>
+						</tr>
+						<tr>
+							<td class="topic">Top Speed :  </td>
+							<td class="descrip">&nbsp 116km/h</td>
+						</tr>
+						<tr>
+							<td class="topic">Doors :  </td>
+							<td class="descrip">&nbsp 2</td>
+						</tr>
+						<tr>
+							<td class="topic">Passengers :  </td>
+							<td class="descrip">&nbsp 4</td>
+						</tr>
+						
+						<tr>
+							<td class="topic">Weight :  </td>
+							<td class="descrip">&nbsp 686kg</td>
+						</tr>
+						<tr>
+							<td class="topic">Length :  </td>
+							<td class="descrip">&nbsp 3054mm</td>
+						</tr>
+					</table>
+				</p>
+				<br><br>
+			</div>					
+			<div class="col-lg-5 col-md-5 single-blog">
+				<img class="img-fluid" src="../image/clminis2000.jpg">
+				<ul class="post-tags">
+					<li>$16,500</li>
+					<li>2000</li>
+					<li>UK</li>
+				</ul>
+				<h4 class="headline">mini cooper s</h4>
+				<p>
+					<table>
+						
+						<tr>
+							<td class="topic">Engine : </td>
+							<td td class="descrip">&nbsp&nbsp2.0l V6</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Engine Capacity : </td>
+							<td td class="descrip">&nbsp&nbsp1275cc</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Fuel Type :  </td>
+							<td class="descrip">&nbsp Petrol(Gasoline)</td>
+						</tr>
+						<tr>
+							<td class="topic">Transmission :  </td>
+							<td class="descrip">&nbsp 4 Speed Manual</td>
+						</tr>
+						<tr>
+							<td class="topic">Fuel Tank Capacity :  </td>
+							<td class="descrip">&nbsp 34l</td>
+						</tr>
+						<tr>
+							<td class="topic">Top Speed :  </td>
+							<td class="descrip">&nbsp 126km/h</td>
+						</tr>
+						<tr>
+							<td class="topic">Doors :  </td>
+							<td class="descrip">&nbsp 2</td>
+						</tr>
+						<tr>
+							<td class="topic">Passengers :  </td>
+							<td class="descrip">&nbsp 5 </td>
+						</tr>
+						
+						<tr>
+							<td class="topic">Weight :  </td>
+							<td class="descrip">&nbsp 1315kg</td>
+						</tr>
+						<tr>
+							<td class="topic">Length :  </td>
+							<td class="descrip">&nbsp 4630mm</td>
+						</tr>
+					</table>
+				</p>
+				<br><br>
+			</div>	
+			<div class="col-lg-7 col-md-5 single-blog">
+				<img class="img-fluid" src="../image/clminiaustin.jpg">
+				<ul class="post-tags">
+					<li>$13,015</li>
+					<li>19963</li>
+					<li>UK</li>
+				</ul>
+				<h4 class="headline"> Austin Mini Cooper </h4>
+				<p>
+					<table>
+						
+
+						<tr>
+							<td class="topic">Engine : </td>
+							<td td class="descrip">&nbsp&nbsp1.3L </td>
+						</tr>
+
+						<tr>
+							<td class="topic">Engine Capacity : </td>
+							<td td class="descrip">&nbsp&nbsp1275cc</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Fuel Type :  </td>
+							<td class="descrip">&nbsp Petrol </td>
+						</tr>
+						<tr>
+							<td class="topic">Transmission :  </td>
+							<td class="descrip">&nbsp 4 Speed Manual</td>
+						</tr>
+						<tr>
+							<td class="topic">Fuel Tank Capacity :  </td>
+							<td class="descrip">&nbsp 25l</td>
+						</tr>
+						<tr>
+							<td class="topic">Top Speed :  </td>
+							<td class="descrip">&nbsp 116km/h</td>
+						</tr>
+						<tr>
+							<td class="topic">Doors :  </td>
+							<td class="descrip">&nbsp 2</td>
+						</tr>
+						<tr>
+							<td class="topic">Passengers :  </td>
+							<td class="descrip">&nbsp 5</td>
+						</tr>
+						
+						<tr>
+							<td class="topic">Weight :  </td>
+							<td class="descrip">&nbsp 585kg</td>
+						</tr>
+						<tr>
+							<td class="topic">Length :  </td>
+							<td class="descrip">&nbsp 3601mm</td>
+						</tr>
+					</table>
+				</p>
+				<br><br>
+			</div>		
+			<div class="col-lg-5 col-md-5 single-blog">
+				<img class="img-fluid" src="../image/clmini2011.jfif">
+				<ul class="post-tags">
+					<li>$31,900 </li>
+					<li>2012</li>
+					<li>UK</li>
+				</ul>
+				<h4 class="headline">Mini John Cooper </h4>
+				<p>
+					<table>
+						
+
+						<tr>
+							<td class="topic">Engine : </td>
+							<td td class="descrip">&nbsp&nbsp1.6L I4</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Engine Capacity : </td>
+							<td td class="descrip">&nbsp&nbsp1071cc</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Fuel Type :  </td>
+							<td class="descrip">&nbsp Premium Gasoline </td>
+						</tr>
+						<tr>
+							<td class="topic">Transmission :  </td>
+							<td class="descrip">&nbsp 6 Speed Manual</td>
+						</tr>
+						<tr>
+							<td class="topic">Fuel Tank Capacity :  </td>
+							<td class="descrip">&nbsp 50l</td>
+						</tr>
+						<tr>
+							<td class="topic">Top Speed :  </td>
+							<td class="descrip">&nbsp 240km/h</td>
+						</tr>
+						<tr>
+							<td class="topic">Doors :  </td>
+							<td class="descrip">&nbsp 2</td>
+						</tr>
+						<tr>
+							<td class="topic">Passengers :  </td>
+							<td class="descrip">&nbsp 4</td>
+						</tr>
+						
+						<tr>
+							<td class="topic">Weight :  </td>
+							<td class="descrip">&nbsp 1228kg</td>
+						</tr>
+						<tr>
+							<td class="topic">Length :  </td>
+							<td class="descrip">&nbsp 3729m</td>
+						</tr>
+					</table>
+				</p>
+                <br><br>
+				</div>	
+            
+		</div>
+	</div>	
+</section>
+<!-- End blog Area -->
+<!-- Footer -->
+	
+<?php include "footer.php" ?>
+
+
+<script src="../js/jquery-3.2.1.min.js"></script>
+<script src="../styles/bootstrap4/popper.js"></script>
+<script src="../styles/bootstrap4/bootstrap.min.js"></script>
+<script src="../plugins/greensock/TweenMax.min.js"></script>
+<script src="../plugins/greensock/TimelineMax.min.js"></script>
+<script src="../plugins/scrollmagic/ScrollMagic.min.js"></script>
+<script src="../plugins/greensock/animation.gsap.min.js"></script>
+<script src="../plugins/greensock/ScrollToPlugin.min.js"></script>
+<script src="../plugins/OwlCarousel2-2.2.1/owl.carousel.js"></script>
+<script src="../plugins/easing/easing.js"></script>
+<script src="../plugins/parallax-js-master/parallax.min.js"></script>
+<script src="../js/about_custom.js"></script>
+
+</body>
+
+</html>
