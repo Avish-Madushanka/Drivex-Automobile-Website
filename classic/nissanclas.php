fdc11b5943cd9756c489d16b0501ba3849b424cf
Avish Madushanka <avishmadushanka21@gmail.com>
2024-02-09 21:18:25 +0530

    Update nissanclas.php

--- classic/nissanclas.php
+++ classic/nissanclas.php
@@ -1,1 +1,433 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title> Nissan Classic Cars</title>
+<link rel="icon" type="image/x-icon" href="/image/Drivex.png">
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="description" content="DriveX">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="../styles/bootstrap4/bootstrap.min.css">
+<link href="../plugins/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" type="text/css" href="../plugins/OwlCarousel2-2.2.1/owl.carousel.css">
+<link rel="stylesheet" type="text/css" href="../plugins/OwlCarousel2-2.2.1/owl.theme.default.css">
+<link rel="stylesheet" type="text/css" href="../plugins/OwlCarousel2-2.2.1/animate.css">
+<link rel="stylesheet" type="text/css" href="../styles/classiccars.css">
+<link rel="stylesheet" type="text/css" href="../styles/about_responsive.css">
+</head>
 
+<body>
+
+<div class="super_container">
+	
+	<!-- Header -->
+
+		<!-- Header -->
+
+		<header class="header">
+<?php include "headerclss.php"?>
+
+</header>
+
+
+	<!-- Intro -->
+
+	<div class="intro">
+		<div class="container">
+		
+				</div>
+				
+					<div class="intro_content">
+						<div class="intro_title"><center>Nissan Classic Cars</center></div>
+						
+						<div><div></div><span></span><span></span><span></span></a></div>
+					</div>
+				</div>
+				
+
+			</div>
+		</div>
+	</div>
+<!-- Start blog Area -->
+<section class="blog-area section-gap" id="blog">
+	<div class="container">
+		<div class="row">
+				<div class="col-lg-7 col-md-5 single-blog">
+				<img class="img-fluid" src="../image/clnischerry.jpg">
+				<ul class="post-tags">
+					<li>$14,500</li>
+					<li>1970</li>
+					<li>Japan</li>
+					
+				</ul>
+				<h4 class="headline">Nissan Cherry </h4>
+				<p>
+					<table>
+						
+						<tr>
+							<td class="topic">Engine : </td>
+							<td td class="descrip">&nbsp&nbsp1.2l V6</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Engine Capacity : </td>
+							<td td class="descrip">&nbsp&nbsp1171cc</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Fuel Type :  </td>
+							<td class="descrip">&nbsp Petrol</td>
+						</tr>
+						<tr>
+							<td class="topic">Transmission :  </td>
+							<td class="descrip">&nbsp 4 Speed Manual</td>
+						</tr>
+						<tr>
+							<td class="topic">Fuel Tank Capacity :  </td>
+							<td class="descrip">&nbsp 50l</td>
+						</tr>
+						<tr>
+							<td class="topic">Top Speed :  </td>
+							<td class="descrip">&nbsp 165km/h</td>
+						</tr>
+						<tr>
+							<td class="topic">Doors :  </td>
+							<td class="descrip">&nbsp 2 or 4</td>
+						</tr>
+						<tr>
+							<td class="topic">Passengers :  </td>
+							<td class="descrip">&nbsp 2</td>
+						</tr>
+						
+						<tr>
+							<td class="topic">Weight :  </td>
+							<td class="descrip">&nbsp 670kg</td>
+						</tr>
+						<tr>
+							<td class="topic">Length :  </td>
+							<td class="descrip">&nbsp 3660mm</td>
+						</tr>
+					</table>
+				</p>
+				<br><br>
+			</div>					
+			<div class="col-lg-5 col-md-5 single-blog">
+				<img class="img-fluid" src="../image/clnisleo.jpg">
+				<ul class="post-tags">
+					<li>$24,750</li>
+					<li>1980</li>
+					<li>Japan</li>
+				</ul>
+				<h4 class="headline">Nissan Leopard F30</h4>
+				<p>
+					<table>
+						
+						<tr>
+							<td class="topic">Engine : </td>
+							<td td class="descrip">&nbsp&nbsp3.0l V6</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Engine Capacity : </td>
+							<td td class="descrip">&nbsp&nbsp1770cc</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Fuel Type :  </td>
+							<td class="descrip">&nbsp Petrol</td>
+						</tr>
+						<tr>
+							<td class="topic">Transmission :  </td>
+							<td class="descrip">&nbsp 5 Speed Manual</td>
+						</tr>
+						<tr>
+							<td class="topic">Fuel Tank Capacity :  </td>
+							<td class="descrip">&nbsp 65l</td>
+						</tr>
+						<tr>
+							<td class="topic">Top Speed :  </td>
+							<td class="descrip">&nbsp 240km/h</td>
+						</tr>
+						<tr>
+							<td class="topic">Doors :  </td>
+							<td class="descrip">&nbsp 4</td>
+						</tr>
+						<tr>
+							<td class="topic">Passengers :  </td>
+							<td class="descrip">&nbsp 5 </td>
+						</tr>
+						
+						<tr>
+							<td class="topic">Weight :  </td>
+							<td class="descrip">&nbsp 1315kg</td>
+						</tr>
+						<tr>
+							<td class="topic">Length :  </td>
+							<td class="descrip">&nbsp 4630mm</td>
+						</tr>
+					</table>
+				</p>
+				<br><br>
+			</div>	
+			<div class="col-lg-7 col-md-5 single-blog">
+				<img class="img-fluid" src="../image/clnis200sx.jpg">
+				<ul class="post-tags">
+					<li>$16,010</li>
+					<li>1994</li>
+					<li>Japan</li>
+				</ul>
+				<h4 class="headline">Nissan 200SX</h4>
+				<p>
+					<table>
+						
+
+						<tr>
+							<td class="topic">Engine : </td>
+							<td td class="descrip">&nbsp&nbsp1.6L V6</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Engine Capacity : </td>
+							<td td class="descrip">&nbsp&nbsp2960cc</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Fuel Type :  </td>
+							<td class="descrip">&nbsp Petrol </td>
+						</tr>
+						<tr>
+							<td class="topic">Transmission :  </td>
+							<td class="descrip">&nbsp 4 Speed Manual</td>
+						</tr>
+						<tr>
+							<td class="topic">Fuel Tank Capacity :  </td>
+							<td class="descrip">&nbsp 50l</td>
+						</tr>
+						<tr>
+							<td class="topic">Top Speed :  </td>
+							<td class="descrip">&nbsp 235km/h</td>
+						</tr>
+						<tr>
+							<td class="topic">Doors :  </td>
+							<td class="descrip">&nbsp 2</td>
+						</tr>
+						<tr>
+							<td class="topic">Passengers :  </td>
+							<td class="descrip">&nbsp 2</td>
+						</tr>
+						
+						<tr>
+							<td class="topic">Weight :  </td>
+							<td class="descrip">&nbsp 1250kg</td>
+						</tr>
+						<tr>
+							<td class="topic">Length :  </td>
+							<td class="descrip">&nbsp 4460mm</td>
+						</tr>
+					</table>
+				</p>
+				<br><br>
+			</div>		
+			<div class="col-lg-5 col-md-5 single-blog">
+				<img class="img-fluid" src="../image/clnissunny.jpg">
+				<ul class="post-tags">
+					<li>$11,300 </li>
+					<li>1982</li>
+					<li>Japan</li>
+				</ul>
+				<h4 class="headline">Nissan Sunny B11</h4>
+				<p>
+					<table>
+						
+
+						<tr>
+							<td class="topic">Engine : </td>
+							<td td class="descrip">&nbsp&nbsp1.3L I4</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Engine Capacity : </td>
+							<td td class="descrip">&nbsp&nbsp1270cc</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Fuel Type :  </td>
+							<td class="descrip">&nbsp Petrol</td>
+						</tr>
+						<tr>
+							<td class="topic">Transmission :  </td>
+							<td class="descrip">&nbsp 5 Speed Manual</td>
+						</tr>
+						<tr>
+							<td class="topic">Fuel Tank Capacity :  </td>
+							<td class="descrip">&nbsp 50l</td>
+						</tr>
+						<tr>
+							<td class="topic">Top Speed :  </td>
+							<td class="descrip">&nbsp 150km/h</td>
+						</tr>
+						<tr>
+							<td class="topic">Doors :  </td>
+							<td class="descrip">&nbsp 4</td>
+						</tr>
+						<tr>
+							<td class="topic">Passengers :  </td>
+							<td class="descrip">&nbsp 5</td>
+						</tr>
+						
+						<tr>
+							<td class="topic">Weight :  </td>
+							<td class="descrip">&nbsp 950kg</td>
+						</tr>
+						<tr>
+							<td class="topic">Length :  </td>
+							<td class="descrip">&nbsp 4255mm</td>
+						</tr>
+					</table>
+				</p>
+                <br><br>
+				</div>	
+                
+			<div class="col-lg-7 col-md-5 single-blog">
+				<img class="img-fluid" src="../image/clnissky.webp" alt="">
+				<ul class="post-tags">
+					<li>$94,363</li>
+					<li>1966</li>
+					<li>Japan</li>
+				</ul>
+				<h4 class="headline">Nissan Skyline </h4>
+				<p>
+					<table>
+						
+
+						<tr>
+							<td class="topic">Engine : </td>
+							<td td class="descrip">&nbsp&nbsp1.9L I4</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Engine Capacity : </td>
+							<td td class="descrip">&nbsp&nbsp1484cc</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Fuel Type :  </td>
+							<td class="descrip">&nbsp Petrol (Gasoline) </td>
+						</tr>
+						<tr>
+							<td class="topic">Transmission :  </td>
+							<td class="descrip">&nbsp 4 Speed Manual</td>
+						</tr>
+						<tr>
+							<td class="topic">Fuel Tank Capacity :  </td>
+							<td class="descrip">&nbsp 40l</td>
+						</tr>
+						<tr>
+							<td class="topic">Top Speed :  </td>
+							<td class="descrip">&nbsp 100km/h</td>
+						</tr>
+						<tr>
+							<td class="topic">Doors :  </td>
+							<td class="descrip">&nbsp </td>
+						</tr>
+						<tr>
+							<td class="topic">Passengers :  </td>
+							<td class="descrip">&nbsp 5</td>
+						</tr>
+						
+						<tr>
+							<td class="topic">Weight :  </td>
+							<td class="descrip">&nbsp 1340kg</td>
+						</tr>
+						<tr>
+							<td class="topic">Length :  </td>
+							<td class="descrip">&nbsp 4100mm</td>
+						</tr>
+					</table>
+				</p>
+				<br><br>
+			</div>		
+			<div class="col-lg-5 col-md-5 single-blog">
+				<img class="img-fluid" src="../image/clniss40.webp">
+				<ul class="post-tags">
+					<li>$29,909</li>
+					<li>1973</li>
+					<li>Japan</li>
+				</ul>
+				<h4 class="headline">Nissan S30</h4>
+				<p>
+					<table>
+						
+
+						<tr>
+							<td class="topic">Engine : </td>
+							<td td class="descrip">&nbsp&nbsp3.0L</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Engine Capacity : </td>
+							<td td class="descrip">&nbsp&nbsp2393cc</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Fuel Type :  </td>
+							<td class="descrip">&nbsp Petrol</td>
+						</tr>
+						<tr>
+							<td class="topic">Transmission :  </td>
+							<td class="descrip">&nbsp 5 Speed Manual</td>
+						</tr>
+						<tr>
+							<td class="topic">Fuel Tank Capacity :  </td>
+							<td class="descrip">&nbsp 60l</td>
+						</tr>
+						<tr>
+							<td class="topic">Top Speed :  </td>
+							<td class="descrip">&nbsp 160km/h</td>
+						</tr>
+						<tr>
+							<td class="topic">Doors :  </td>
+							<td class="descrip">&nbsp 2</td>
+						</tr>
+						<tr>
+							<td class="topic">Passengers :  </td>
+							<td class="descrip">&nbsp 2</td>
+						</tr>
+						
+						<tr>
+							<td class="topic">Weight :  </td>
+							<td class="descrip">&nbsp 1190kg</td>
+						</tr>
+						<tr>
+							<td class="topic">Length :  </td>
+							<td class="descrip">&nbsp 4690mm</td>
+						</tr>
+					</table>
+				</p>
+                <br><br>
+				</div>	
+		</div>
+	</div>	
+</section>
+<!-- End blog Area -->
+<!-- Footer -->
+	
+<?php include "footer.php" ?>
+
+
+<script src="../js/jquery-3.2.1.min.js"></script>
+<script src="../styles/bootstrap4/popper.js"></script>
+<script src="../styles/bootstrap4/bootstrap.min.js"></script>
+<script src="../plugins/greensock/TweenMax.min.js"></script>
+<script src="../plugins/greensock/TimelineMax.min.js"></script>
+<script src="../plugins/scrollmagic/ScrollMagic.min.js"></script>
+<script src="../plugins/greensock/animation.gsap.min.js"></script>
+<script src="../plugins/greensock/ScrollToPlugin.min.js"></script>
+<script src="../plugins/OwlCarousel2-2.2.1/owl.carousel.js"></script>
+<script src="../plugins/easing/easing.js"></script>
+<script src="../plugins/parallax-js-master/parallax.min.js"></script>
+<script src="../js/about_custom.js"></script>
+
+</body>
+
+</html>
