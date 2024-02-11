bf016cbb6e6e1d84a38bc9fb367ff65e252ad87b
Avish Madushanka <avishmadushanka21@gmail.com>
2024-02-11 21:17:56 +0530

    Update porscheclas.php

--- classic/porscheclas.php
+++ classic/porscheclas.php
@@ -1,1 +1,432 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Porsche Classic Cars</title>
+<link rel="icon" type="image/x-icon" href="/image/Drivex.png">
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="description" content="DriveX">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="../styles/bootstrap4/bootstrap.min.css">
+<link href="../plugins/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" type="text/css" href="../plugins/OwlCarousel2-2.2.1/owl.carousel.css">
+<link rel="stylesheet" type="text/css" href="../plugins/OwlCarousel2-2.2.1/owl.theme.default.css">
+<link rel="stylesheet" type="text/css" href="../plugins/OwlCarousel2-2.2.1/animate.css">
+<link rel="stylesheet" type="text/css" href="../styles/classiccars.css">
+<link rel="stylesheet" type="text/css" href="../styles/about_responsive.css">
+</head>
 
+<body>
+
+<div class="super_container">
+	
+		<!-- Header -->
+
+		<header class="header">
+<?php include "headerclss.php"?>
+
+</header>
+
+	<!-- Intro -->
+
+	<div class="intro">
+		<div class="container">
+		
+				</div>
+				
+					<div class="intro_content">
+						<div class="intro_title"><center>Porsche Classic Cars</center></div>
+						
+						<div><div></div><span></span><span></span><span></span></a></div>
+					</div>
+				</div>
+				
+
+			</div>
+		</div>
+	</div>
+<!-- Start blog Area -->
+<section class="blog-area section-gap" id="blog">
+	<div class="container">
+		<div class="row">
+				<div class="col-lg-7 col-md-5 single-blog">
+				<img class="img-fluid" src="../image/clporsche356.jpg" alt="">
+				<ul class="post-tags">
+					<li>$75,000</li>
+					<li>1948</li>
+					<li>Germany</li>
+					
+				</ul>
+				<h4 class="headline">Porsche 356</h4>
+				<p>
+					<table>
+						
+						<tr>
+							<td class="topic">Engine : </td>
+							<td td class="descrip">&nbsp&nbsp2.7L</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Engine Capacity : </td>
+							<td td class="descrip">&nbsp&nbsp1588cc</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Fuel Type :  </td>
+							<td class="descrip">&nbsp Petrol (Gasoline)</td>
+						</tr>
+						<tr>
+							<td class="topic">Transmission :  </td>
+							<td class="descrip">&nbsp 5 Speed Manual</td>
+						</tr>
+						<tr>
+							<td class="topic">Fuel Tank Capacity :  </td>
+							<td class="descrip">&nbsp 52l</td>
+						</tr>
+						<tr>
+							<td class="topic">Top Speed :  </td>
+							<td class="descrip">&nbsp 160km/h</td>
+						</tr>
+						<tr>
+							<td class="topic">Doors :  </td>
+							<td class="descrip">&nbsp 2</td>
+						</tr>
+						<tr>
+							<td class="topic">Passengers :  </td>
+							<td class="descrip">&nbsp 4</td>
+						</tr>
+						
+						<tr>
+							<td class="topic">Weight :  </td>
+							<td class="descrip">&nbsp 1041kg</td>
+						</tr>
+						<tr>
+							<td class="topic">Length :  </td>
+							<td class="descrip">&nbsp 4010mm</td>
+						</tr>
+					</table>
+				</p>
+				<br><br>
+			</div>					
+			<div class="col-lg-5 col-md-5 single-blog">
+				<img class="img-fluid" src="../image/clporsche914.jpg" alt="">
+				<ul class="post-tags">
+					<li>$40,042</li>
+					<li>1976</li>
+					<li>Germany</li>
+				</ul>
+				<h4 class="headline">Porsche 914</h4>
+				<p>
+					<table>
+						
+						<tr>
+							<td class="topic">Engine : </td>
+							<td td class="descrip">&nbsp&nbsp2.7l</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Engine Capacity : </td>
+							<td td class="descrip">&nbsp&nbsp1679cc</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Fuel Type :  </td>
+							<td class="descrip">&nbsp Petrol</td>
+						</tr>
+						<tr>
+							<td class="topic">Transmission :  </td>
+							<td class="descrip">&nbsp 5 Speed Manual</td>
+						</tr>
+						<tr>
+							<td class="topic">Fuel Tank Capacity :  </td>
+							<td class="descrip">&nbsp 63l</td>
+						</tr>
+						<tr>
+							<td class="topic">Top Speed :  </td>
+							<td class="descrip">&nbsp 250km/h</td>
+						</tr>
+						<tr>
+							<td class="topic">Doors :  </td>
+							<td class="descrip">&nbsp 2</td>
+						</tr>
+						<tr>
+							<td class="topic">Passengers :  </td>
+							<td class="descrip">&nbsp 2</td>
+						</tr>
+						
+						<tr>
+							<td class="topic">Weight :  </td>
+							<td class="descrip">&nbsp 995kg</td>
+						</tr>
+						<tr>
+							<td class="topic">Length :  </td>
+							<td class="descrip">&nbsp 3985mm</td>
+						</tr>
+					</table>
+				</p>
+				<br><br>
+			</div>	
+			<div class="col-lg-7 col-md-5 single-blog">
+				<img class="img-fluid" src="../image/clporsche74.png" alt="">
+				<ul class="post-tags">
+					<li>$195,000</li>
+					<li>1970</li>
+					<li>Germany</li>
+				</ul>
+				<h4 class="headline">Porsche Carrera 911</h4>
+				<p>
+					<table>
+						
+
+						<tr>
+							<td class="topic">Engine : </td>
+							<td td class="descrip">&nbsp&nbsp2.0l</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Engine Capacity : </td>
+							<td td class="descrip">&nbsp&nbsp1991cc</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Fuel Type :  </td>
+							<td class="descrip">&nbsp Petrol (Gasoline)</td>
+						</tr>
+						<tr>
+							<td class="topic">Transmission :  </td>
+							<td class="descrip">&nbsp 5 Speed Manual</td>
+						</tr>
+						<tr>
+							<td class="topic">Fuel Tank Capacity :  </td>
+							<td class="descrip">&nbsp 80l</td>
+						</tr>
+						<tr>
+							<td class="topic">Top Speed :  </td>
+							<td class="descrip">&nbsp 293km/h</td>
+						</tr>
+						<tr>
+							<td class="topic">Doors :  </td>
+							<td class="descrip">&nbsp 2</td>
+						</tr>
+						<tr>
+							<td class="topic">Passengers :  </td>
+							<td class="descrip">&nbsp 2</td>
+						</tr>
+						
+						<tr>
+							<td class="topic">Weight :  </td>
+							<td class="descrip">&nbsp 1129kg</td>
+						</tr>
+						<tr>
+							<td class="topic">Length :  </td>
+							<td class="descrip">&nbsp 4290mm</td>
+						</tr>
+					</table>
+				</p>
+				<br><br>
+			</div>		
+			<div class="col-lg-5 col-md-5 single-blog">
+				<img class="img-fluid" src="../image/clporsche86.jpg" alt="">
+				<ul class="post-tags">
+					<li>$25,581 </li>
+					<li>1986</li>
+					<li>Germany</li>
+				</ul>
+				<h4 class="headline">Porsche Turbo 944</h4>
+				<p>
+					<table>
+						
+
+						<tr>
+							<td class="topic">Engine : </td>
+							<td td class="descrip">&nbsp&nbsp2.5l I4</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Engine Capacity : </td>
+							<td td class="descrip">&nbsp&nbsp1968cc</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Fuel Type :  </td>
+							<td class="descrip">&nbsp Petrol</td>
+						</tr>
+						<tr>
+							<td class="topic">Transmission :  </td>
+							<td class="descrip">&nbsp 3 Speed Automatic</td>
+						</tr>
+						<tr>
+							<td class="topic">Fuel Tank Capacity :  </td>
+							<td class="descrip">&nbsp 96l</td>
+						</tr>
+						<tr>
+							<td class="topic">Top Speed :  </td>
+							<td class="descrip">&nbsp 220km/h</td>
+						</tr>
+						<tr>
+							<td class="topic">Doors :  </td>
+							<td class="descrip">&nbsp 2</td>
+						</tr>
+						<tr>
+							<td class="topic">Passengers :  </td>
+							<td class="descrip">&nbsp 2</td>
+						</tr>
+						
+						<tr>
+							<td class="topic">Weight :  </td>
+							<td class="descrip">&nbsp 1180kg</td>
+						</tr>
+						<tr>
+							<td class="topic">Length :  </td>
+							<td class="descrip">&nbsp 4318mm</td>
+						</tr>
+					</table>
+				</p>
+                <br><br>
+				
+			</div>						
+			
+			<div class="col-lg-7 col-md-5 single-blog">
+				<img class="img-fluid" src="../image/clporsche99.jpg" alt="">
+				<ul class="post-tags">
+					<li>$195,000</li>
+					<li>1999</li>
+					<li>Germany</li>
+				</ul>
+				<h4 class="headline">Porsche Boxster S</h4>
+				<p>
+					<table>
+						
+
+						<tr>
+							<td class="topic">Engine : </td>
+							<td td class="descrip">&nbsp&nbsp3.2l</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Engine Capacity : </td>
+							<td td class="descrip">&nbsp&nbsp2687cc</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Fuel Type :  </td>
+							<td class="descrip">&nbsp Petrol (Gasoline)</td>
+						</tr>
+						<tr>
+							<td class="topic">Transmission :  </td>
+							<td class="descrip">&nbsp 5 Speed Manual</td>
+						</tr>
+						<tr>
+							<td class="topic">Fuel Tank Capacity :  </td>
+							<td class="descrip">&nbsp 64l</td>
+						</tr>
+						<tr>
+							<td class="topic">Top Speed :  </td>
+							<td class="descrip">&nbsp 160km/h</td>
+						</tr>
+						<tr>
+							<td class="topic">Doors :  </td>
+							<td class="descrip">&nbsp 2</td>
+						</tr>
+						<tr>
+							<td class="topic">Passengers :  </td>
+							<td class="descrip">&nbsp 2</td>
+						</tr>
+						
+						<tr>
+							<td class="topic">Weight :  </td>
+							<td class="descrip">&nbsp 1320kg</td>
+						</tr>
+						<tr>
+							<td class="topic">Length :  </td>
+							<td class="descrip">&nbsp 4343mm</td>
+						</tr>
+					</table>
+				</p>
+				<br><br>
+			</div>	
+			<div class="col-lg-5 col-md-5 single-blog">
+				<img class="img-fluid" src="../image/clporsche98.jpg" alt="">
+				<ul class="post-tags">
+					<li>$29,585</li>
+					<li>1998</li>
+					<li>Germany</li>
+
+				</ul>
+				<h4 class="headline">Porsche Carrera 911</h4>
+				<p>
+					<table>
+						
+						<tr>
+							<td class="topic">Engine : </td>
+							<td td class="descrip">&nbsp&nbsp3.6l</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Engine Capacity : </td>
+							<td class="descrip">&nbsp&nbsp3600cc</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Fuel Type :  </td>
+							<td class="descrip">&nbsp Petrol </td>
+						</tr>
+						<tr>
+							<td class="topic">Transmission :  </td>
+							<td class="descrip">&nbsp 6 Speed Manual</td>
+						</tr>
+						<tr>
+							<td class="topic">Fuel Tank Capacity :  </td>
+							<td class="descrip">&nbsp 75l</td>
+						</tr>
+						<tr>
+							<td class="topic">Top Speed :  </td>
+							<td class="descrip">&nbsp 276km/h</td>
+						</tr>
+						<tr>
+							<td class="topic">Doors :  </td>
+							<td class="descrip">&nbsp 2</td>
+						</tr>
+						<tr>
+							<td class="topic">Passengers :  </td>
+							<td class="descrip">&nbsp 2</td>
+						</tr>
+						
+						<tr>
+							<td class="topic">Weight :  </td>
+							<td class="descrip">&nbsp 1375kg</td>
+						</tr>
+						<tr>
+							<td class="topic">Length :  </td>
+							<td class="descrip">&nbsp 4260mm</td>
+						</tr>
+					</table>
+				</p>
+				<br><br>
+			</div>	
+			
+		</div>
+	</div>	
+</section>
+<!-- End blog Area -->
+<!-- Footer -->
+	
+<?php include "footer.php" ?>
+
+
+<script src="../js/jquery-3.2.1.min.js"></script>
+<script src="../styles/bootstrap4/popper.js"></script>
+<script src="../styles/bootstrap4/bootstrap.min.js"></script>
+<script src="../plugins/greensock/TweenMax.min.js"></script>
+<script src="../plugins/greensock/TimelineMax.min.js"></script>
+<script src="../plugins/scrollmagic/ScrollMagic.min.js"></script>
+<script src="../plugins/greensock/animation.gsap.min.js"></script>
+<script src="../plugins/greensock/ScrollToPlugin.min.js"></script>
+<script src="../plugins/OwlCarousel2-2.2.1/owl.carousel.js"></script>
+<script src="../plugins/easing/easing.js"></script>
+<script src="../plugins/parallax-js-master/parallax.min.js"></script>
+<script src="../js/about_custom.js"></script>
+
+</body>
+
+</html>
