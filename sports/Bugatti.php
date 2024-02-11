242cc23671d33c5b90ca52215caa62d0d66c1f00
Avish Madushanka <avishmadushanka21@gmail.com>
2024-02-11 21:19:27 +0530

    Update Bugatti.php

--- sports/Bugatti.php
+++ sports/Bugatti.php
@@ -1,1 +1,372 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Bugatti Sport Cars</title>
+<link rel="icon" type="image/x-icon" href="/image/Drivex.png">
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="description" content="DriveX">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="../styles/bootstrap4/bootstrap.min.css">
+<link href="../plugins/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" type="text/css" href="../plugins/OwlCarousel2-2.2.1/owl.carousel.css">
+<link rel="stylesheet" type="text/css" href="../plugins/OwlCarousel2-2.2.1/owl.theme.default.css">
+<link rel="stylesheet" type="text/css" href="../plugins/OwlCarousel2-2.2.1/animate.css">
+<link rel="stylesheet" type="text/css" href="../styles/classiccars.css">
+<link rel="stylesheet" type="text/css" href="../styles/about_responsive.css">
+</head>
 
+<body>
+
+<div class="super_container">
+	
+	<!-- Header -->
+
+	<header class="header">
+<?php include "header.php"?>
+
+</header>
+
+	<!-- Intro -->
+
+	<div class="intro">
+		<div class="container">
+		
+				</div>
+				
+					<div class="intro_content">
+						<div class="intro_title"><center>Bugatti Sport Cars</center></div>
+						
+						<div><div></div><span></span><span></span><span></span></a></div>
+					</div>
+				</div>
+				
+
+			</div>
+		</div>
+	</div>
+<!-- Start blog Area -->
+<section class="blog-area section-gap" id="blog">
+	<div class="container">
+		<div class="row">
+				<div class="col-lg-7 col-md-5 single-blog">
+				<img class="img-fluid" src="../image/bugattive.jpg">
+				<ul class="post-tags">
+					<li>$1,700,000</li>
+					<li>$2005</li>
+					<li>France</li>
+					
+				</ul>
+				<h4 class="headline"> Veyron </h4>
+				<p>
+					<table>
+						
+						<tr>
+							<td class="topic">Engine : </td>
+							<td td class="descrip">&nbsp&nbsp8.0l W12</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Engine Capacity : </td>
+							<td td class="descrip">&nbsp&nbsp7993cc</td>
+						</tr>
+
+                        <tr>
+							<td class="topic">Torque : </td>
+							<td td class="descrip">&nbsp&nbsp1250Nm@6500rpm</td>
+						</tr>
+                        
+                        <tr>
+							<td class="topic">Fuel Type :  </td>
+							<td class="descrip">&nbsp Petrol</td>
+						</tr>
+						<tr>
+							<td class="topic">Transmission :  </td>
+							<td class="descrip">&nbsp 7 Speed Automatic</td>
+						</tr>
+						<tr>
+							<td class="topic">Power :  </td>
+							<td class="descrip">&nbsp 1001bhp</td>
+						</tr>
+						<tr>
+							<td class="topic">Top Speed :  </td>
+							<td class="descrip">&nbsp 431km/h</td>
+						</tr>
+                        <tr>
+							<td class="topic">Acceleration 0-100 km/h : </td>
+							<td td class="descrip">&nbsp&nbsp2.7s</td>
+						</tr>
+                        <tr>
+							<td class="topic">Chassis :  </td>
+							<td class="descrip">&nbsp Carbonfibre Monocoque</td>
+						</tr>
+                        <tr>
+							<td class="topic">No of Doors : </td>
+							<td td class="descrip">&nbsp&nbsp2</td>
+						</tr>
+                        <tr>
+							<td class="topic">Weight :  </td>
+							<td class="descrip">&nbsp 1888kg</td>
+						</tr>
+						<tr>
+							<td class="topic">Length :  </td>
+							<td class="descrip">&nbsp 4462mm</td>
+						</tr>
+					</table>
+				</p>
+				<br><br>
+			</div>					
+			<div class="col-lg-5 col-md-5 single-blog">
+				<img class="img-fluid" src="../image/bugattich.jpg" alt="">
+				<ul class="post-tags">
+					<li>$3,825,000</li>
+					<li>2020</li>
+					<li>France</li>
+					
+				</ul>
+				<h4 class="headline"> Chiron </h4>
+				<p>
+					<table>
+						
+						<tr>
+							<td class="topic">Engine : </td>
+							<td td class="descrip">&nbsp&nbsp8.0l W16</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Engine Capacity : </td>
+							<td td class="descrip">&nbsp&nbsp7993cc</td>
+						</tr>
+
+                        <tr>
+							<td class="topic">Torque : </td>
+							<td td class="descrip">&nbsp&nbsp1600Nm@6000rpm</td>
+						</tr>
+                        
+                        <tr>
+							<td class="topic">Fuel Type :  </td>
+							<td class="descrip">&nbsp Petrol</td>
+						</tr>
+						<tr>
+							<td class="topic">Transmission :  </td>
+							<td class="descrip">&nbsp 7 Speed Automatic</td>
+						</tr>
+						<tr>
+							<td class="topic">Power :  </td>
+							<td class="descrip">&nbsp 1479bhp</td>
+						</tr>
+						<tr>
+							<td class="topic">Top Speed :  </td>
+							<td class="descrip">&nbsp 440km/h</td>
+						</tr>
+                        <tr>
+							<td class="topic">Acceleration 0-100 km/h : </td>
+							<td td class="descrip">&nbsp&nbsp2.4s</td>
+						</tr>
+                        <tr>
+							<td class="topic">Chassis :  </td>
+							<td class="descrip">&nbsp Fetherlight Carbonfibre </td>
+						</tr>
+                        <tr>
+							<td class="topic">No of Doors : </td>
+							<td td class="descrip">&nbsp&nbsp2</td>
+						</tr>
+                        <tr>
+							<td class="topic">Weight :  </td>
+							<td class="descrip">&nbsp 1996kg</td>
+						</tr>
+						<tr>
+							<td class="topic">Length :  </td>
+							<td class="descrip">&nbsp 4544mm</td>
+						</tr>
+					</table>
+				</p>
+                <br><br>
+				
+			</div>						
+			
+			
+			<div class="col-lg-7 col-md-5 single-blog">
+				<img class="img-fluid" src="../image/bugatticen.jpg" alt="">
+				<ul class="post-tags">
+					<li>$8,940,000</li>
+					<li>2022</li>
+					<li>France</li>
+					
+				</ul>
+				<h4 class="headline"> Centodieci </h4>
+				<p>
+					<table>
+						
+						<tr>
+							<td class="topic">Engine : </td>
+							<td td class="descrip">&nbsp&nbsp8.0l W16</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Engine Capacity : </td>
+							<td td class="descrip">&nbsp&nbsp7993cc</td>
+						</tr>
+
+                        <tr>
+							<td class="topic">Torque : </td>
+							<td td class="descrip">&nbsp&nbsp1600Nm@6000rpm</td>
+						</tr>
+                        
+                        <tr>
+							<td class="topic">Fuel Type :  </td>
+							<td class="descrip">&nbsp Petrol</td>
+						</tr>
+						<tr>
+							<td class="topic">Transmission :  </td>
+							<td class="descrip">&nbsp 7 Speed Automatic</td>
+						</tr>
+						<tr>
+							<td class="topic">Power :  </td>
+							<td class="descrip">&nbsp 1578bhp</td>
+						</tr>
+						<tr>
+							<td class="topic">Top Speed :  </td>
+							<td class="descrip">&nbsp 380km/h</td>
+						</tr>
+                        <tr>
+							<td class="topic">Acceleration 0-100 km/h : </td>
+							<td td class="descrip">&nbsp&nbsp2.4s</td>
+						</tr>
+                        <tr>
+							<td class="topic">Chassis :  </td>
+							<td class="descrip">&nbsp Fetherlight Carbonfibre </td>
+						</tr>
+                        <tr>
+							<td class="topic">No of Doors : </td>
+							<td td class="descrip">&nbsp&nbsp2</td>
+						</tr>
+                        <tr>
+							<td class="topic">Weight :  </td>
+							<td class="descrip">&nbsp 1975kg</td>
+						</tr>
+						<tr>
+							<td class="topic">Length :  </td>
+							<td class="descrip">&nbsp 4544mm</td>
+						</tr>
+					</table>
+				</p>
+				<br><br>
+			</div>
+			
+			
+			<div class="col-lg-5 col-md-5 single-blog">
+				<img class="img-fluid" src="../image/bugattibo.png" alt="">
+				<ul class="post-tags">
+					<li>$4,713,600</li>
+					<li>2020</li>
+					<li>France</li>
+					
+				</ul>
+				<h4 class="headline">Bolide</h4>
+				<p>
+					<table>
+						
+						<tr>
+							<td class="topic">Engine : </td>
+							<td td class="descrip">&nbsp&nbsp8.0l W16</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Engine Capacity : </td>
+							<td td class="descrip">&nbsp&nbsp7993cc</td>
+						</tr>
+
+                        <tr>
+							<td class="topic">Torque : </td>
+							<td td class="descrip">&nbsp&nbsp1850Nm@7025rpm</td>
+						</tr>
+                        
+                        <tr>
+							<td class="topic">Fuel Type :  </td>
+							<td class="descrip">&nbsp Petrol</td>
+						</tr>
+						<tr>
+							<td class="topic">Transmission :  </td>
+							<td class="descrip">&nbsp 7 Speed Automatic</td>
+						</tr>
+						<tr>
+							<td class="topic">Power :  </td>
+							<td class="descrip">&nbsp 1850bhp</td>
+						</tr>
+						<tr>
+							<td class="topic">Top Speed :  </td>
+							<td class="descrip">&nbsp 498km/h</td>
+						</tr>
+                        <tr>
+							<td class="topic">Acceleration 0-100 km/h : </td>
+							<td td class="descrip">&nbsp&nbsp2.17s</td>
+						</tr>
+                        <tr>
+							<td class="topic">Chassis :  </td>
+							<td class="descrip">&nbsp Titanium & Carbonfibre </td>
+						</tr>
+                        <tr>
+							<td class="topic">No of Doors : </td>
+							<td td class="descrip">&nbsp&nbsp2</td>
+						</tr>
+                        <tr>
+							<td class="topic">Weight :  </td>
+							<td class="descrip">&nbsp 1240kg</td>
+						</tr>
+						<tr>
+							<td class="topic">Length :  </td>
+							<td class="descrip">&nbsp 4756mm</td>
+						</tr>
+					</table>
+				</p>
+				<br><br>
+			</div>
+		</div>
+	</div>	
+</section>
+<!-- End blog Area -->
+<!-- Footer -->
+	
+<footer>
+    <div class="footer-top text-center">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-6 text-center">
+                    <h4 class="navbar-brand">DRIVEX</h4>
+                    <p>We provide information about all types of cars in the world to all car addictors. We provide information about classic cars, luxury cars, sports cars and new arrivals. Also we have a range of articles about cars as well as our social media pages provide you additional insights about all types of cars.</p>
+                    <div class="col-auto social-icons">
+                        <a href="#"><i class='fa fa-facebook-f'></i></a>
+                        <a href="#"><i class='fa fa-twitter'></i></a>
+                        <a href="#"><i class='fa fa-instagram'></i></a>
+                        
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer-bottom text-center">
+        <p class="mb-0"></p> Made With <i class="fa fa-heart-o" aria-hidden="true"></i> By ICONS
+            
+    </div>
+</footer>
+
+
+<script src="../js/jquery-3.2.1.min.js"></script>
+<script src="../styles/bootstrap4/popper.js"></script>
+<script src="../styles/bootstrap4/bootstrap.min.js"></script>
+<script src="../plugins/greensock/TweenMax.min.js"></script>
+<script src="../plugins/greensock/TimelineMax.min.js"></script>
+<script src="../plugins/scrollmagic/ScrollMagic.min.js"></script>
+<script src="../plugins/greensock/animation.gsap.min.js"></script>
+<script src="../plugins/greensock/ScrollToPlugin.min.js"></script>
+<script src="../plugins/OwlCarousel2-2.2.1/owl.carousel.js"></script>
+<script src="../plugins/easing/easing.js"></script>
+<script src="../plugins/parallax-js-master/parallax.min.js"></script>
+<script src="../js/about_custom.js"></script>
+
+
+</body>
+
+</html>
