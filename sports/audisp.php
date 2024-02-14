93c1cc8a68a738a9cca1df40567715bd936a5176
Avish Madushanka <avishmadushanka21@gmail.com>
2024-02-14 18:00:46 +0530

    Create audisp.php

---
+++ sports/audisp.php
@@ -0,0 +1,370 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Audi Sport Cars</title>
+<link rel="icon" type="image/x-icon" href="/image/Drivex.png">
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="description" content="DriveX">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="../styles/bootstrap4/bootstrap.min.css">
+<link href="../plugins/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" type="text/css" href="../plugins/OwlCarousel2-2.2.1/owl.carousel.css">
+<link rel="stylesheet" type="text/css" href="../plugins/OwlCarousel2-2.2.1/owl.theme.default.css">
+<link rel="stylesheet" type="text/css" href="../plugins/OwlCarousel2-2.2.1/animate.css">
+<link rel="stylesheet" type="text/css" href="../styles/classiccars.css">
+<link rel="stylesheet" type="text/css" href="../styles/about_responsive.css">
+</head>
+
+<body>
+
+<div class="super_container">
+	
+	<!-- Header -->
+
+	<header class="header">
+<?php include "header.php"?>
+
+</header>
+
+	<!-- Intro -->
+
+	<div class="intro">
+		<div class="container">
+		
+				</div>
+				
+					<div class="intro_content">
+						<div class="intro_title"><center>Audi Sport Cars</center></div>
+						
+						<div><div></div><span></span><span></span><span></span></a></div>
+					</div>
+				</div>
+				
+
+			</div>
+		</div>
+	</div>
+<!-- Start blog Area -->
+<section class="blog-area section-gap" id="blog">
+	<div class="container">
+		<div class="row">
+				<div class="col-lg-7 col-md-5 single-blog">
+				<img class="img-fluid" src="../image/audiapR8.jpg">
+				<ul class="post-tags">
+					<li>$155,000</li>
+					<li>$2022</li>
+					<li>Germany</li>
+					
+				</ul>
+				<h4 class="headline">Audi R8</h4>
+				<p>
+					<table>
+						
+						<tr>
+							<td class="topic">Engine : </td>
+							<td td class="descrip">&nbsp&nbsp5.2L V10</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Engine Capacity : </td>
+							<td td class="descrip">&nbsp&nbsp5204cc</td>
+						</tr>
+
+                        <tr>
+							<td class="topic">Torque : </td>
+							<td td class="descrip">&nbsp&nbsp602NM@8100RPM</td>
+						</tr>
+                        
+                        <tr>
+							<td class="topic">Fuel Type :  </td>
+							<td class="descrip">&nbsp Petrol</td>
+						</tr>
+						<tr>
+							<td class="topic">Transmission :  </td>
+							<td class="descrip">&nbsp 7 Speed Automatic</td>
+						</tr>
+						<tr>
+							<td class="topic">Power :  </td>
+							<td class="descrip">&nbsp 620hp</td>
+						</tr>
+						<tr>
+							<td class="topic">Top Speed :  </td>
+							<td class="descrip">&nbsp310km/h</td>
+						</tr>
+                        <tr>
+							<td class="topic">Acceleration 0-100 km/h : </td>
+							<td td class="descrip">&nbsp&nbsp3.6s</td>
+						</tr>
+                        <tr>
+							<td class="topic">Chassis :  </td>
+							<td class="descrip">&nbsp Carbonfibre Monocoque</td>
+						</tr>
+                        <tr>
+							<td class="topic">No of Doors : </td>
+							<td td class="descrip">&nbsp&nbsp2</td>
+						</tr>
+                        <tr>
+							<td class="topic">Weight :  </td>
+							<td class="descrip">&nbsp 1650kg</td>
+						</tr>
+						<tr>
+							<td class="topic">Length :  </td>
+							<td class="descrip">&nbsp 4229mm</td>
+						</tr>
+					</table>
+				</p>
+				<br><br>
+			</div>					
+			<div class="col-lg-5 col-md-5 single-blog">
+				<img class="img-fluid" src="../image/audispR35.jpg">
+				<ul class="post-tags">
+					<li>$77,900</li>
+					<li>2021</li>
+					<li>Germany</li>
+					
+				</ul>
+				<h4 class="headline">AUDI RS5</h4>
+				<p>
+					<table>
+						
+						<tr>
+							<td class="topic">Engine : </td>
+							<td td class="descrip">&nbsp&nbsp2.9L 6V</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Engine Capacity : </td>
+							<td td class="descrip">&nbsp&nbsp2894cc</td>
+						</tr>
+
+                        <tr>
+							<td class="topic">Torque : </td>
+							<td td class="descrip">&nbsp&nbsp601Nm@443ft-lb</td>
+						</tr>
+                        
+                        <tr>
+							<td class="topic">Fuel Type :  </td>
+							<td class="descrip">&nbsp Petrol</td>
+						</tr>
+						<tr>
+							<td class="topic">Transmission :  </td>
+							<td class="descrip">&nbsp 8 Speed Sequential</td>
+						</tr>
+						<tr>
+							<td class="topic">Power :  </td>
+							<td class="descrip">&nbsp 444hp</td>
+						</tr>
+						<tr>
+							<td class="topic">Top Speed :  </td>
+							<td class="descrip">&nbsp 250km/h</td>
+						</tr>
+                        <tr>
+							<td class="topic">Acceleration 0-100 km/h : </td>
+							<td td class="descrip">&nbsp&nbsp3.9s</td>
+						</tr>
+                        <tr>
+							<td class="topic">Chassis :  </td>
+							<td class="descrip">&nbsp Fetherlight Carbonfibre </td>
+						</tr>
+                        <tr>
+							<td class="topic">No of Doors : </td>
+							<td td class="descrip">&nbsp&nbsp2</td>
+						</tr>
+                        <tr>
+							<td class="topic">Weight :  </td>
+							<td class="descrip">&nbsp 1810kg</td>
+						</tr>
+						<tr>
+							<td class="topic">Length :  </td>
+							<td class="descrip">&nbsp 4783mm</td>
+						</tr>
+					</table>
+				</p>
+                <br><br>
+				
+			</div>						
+			<div class="col-lg-7 col-md-5 single-blog">
+				<img class="img-fluid" src="../image/audisps5.jpg">
+				<ul class="post-tags">
+					<li>$56,395</li>
+					<li>2022</li>
+					<li>Germany</li>
+					
+				</ul>
+				<h4 class="headline">Audi S5 Sportback</h4>
+				<p>
+					<table>
+						
+						<tr>
+							<td class="topic">Engine : </td>
+							<td td class="descrip">&nbsp&nbsp3.0L V6</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Engine Capacity : </td>
+							<td td class="descrip">&nbsp&nbsp2994cc</td>
+						</tr>
+
+                        <tr>
+							<td class="topic">Torque : </td>
+							<td td class="descrip">&nbsp&nbsp1370rpm@369lb.-ft</td>
+						</tr>
+                        
+                        <tr>
+							<td class="topic">Fuel Type :  </td>
+							<td class="descrip">&nbsp Diesel</td>
+						</tr>
+						<tr>
+							<td class="topic">Transmission :  </td>
+							<td class="descrip">&nbsp 8 Speed Automatic</td>
+						</tr>
+						<tr>
+							<td class="topic">Power :  </td>
+							<td class="descrip">&nbsp 349bhp</td>
+						</tr>
+						<tr>
+							<td class="topic">Top Speed :  </td>
+							<td class="descrip">&nbsp 160km/h</td>
+						</tr>
+                        <tr>
+							<td class="topic">Acceleration 0-100 km/h : </td>
+							<td td class="descrip">&nbsp&nbsp4.8s</td>
+						</tr>
+                        <tr>
+							<td class="topic">Chassis :  </td>
+							<td class="descrip">&nbsp Fetherlight Carbonfibre </td>
+						</tr>
+                        <tr>
+							<td class="topic">No of Doors : </td>
+							<td td class="descrip">&nbsp&nbsp2</td>
+						</tr>
+                        <tr>
+							<td class="topic">Weight :  </td>
+							<td class="descrip">&nbsp 1760kg</td>
+						</tr>
+						<tr>
+							<td class="topic">Length :  </td>
+							<td class="descrip">&nbsp 4765mm</td>
+						</tr>
+					</table>
+				</p>
+				<br><br>
+			</div>
+			
+			
+			<div class="col-lg-5 col-md-5 single-blog">
+				<img class="img-fluid" src="../image/audispR8.jfif">
+				<ul class="post-tags">
+					<li>$505,750</li>
+					<li>2022</li>
+					<li>Germany</li>
+					
+				</ul>
+				<h4 class="headline">Audi GT3</h4>
+				<p>
+					<table>
+						
+						<tr>
+							<td class="topic">Engine : </td>
+							<td td class="descrip">&nbsp&nbsp4.0l 10V</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Engine Capacity : </td>
+							<td td class="descrip">&nbsp&nbsp5200cc</td>
+						</tr>
+
+                        <tr>
+							<td class="topic">Torque : </td>
+							<td td class="descrip">&nbsp&nbsp550Nm@8500rpm</td>
+						</tr>
+                        
+                        <tr>
+							<td class="topic">Fuel Type :  </td>
+							<td class="descrip">&nbsp Petrol</td>
+						</tr>
+						<tr>
+							<td class="topic">Transmission :  </td>
+							<td class="descrip">&nbsp 6 Speed Automatic</td>
+						</tr>
+						<tr>
+							<td class="topic">Power :  </td>
+							<td class="descrip">&nbsp 585hp</td>
+						</tr>
+						<tr>
+							<td class="topic">Top Speed :  </td>
+							<td class="descrip">&nbsp 319km/h</td>
+						</tr>
+                        <tr>
+							<td class="topic">Acceleration 0-100 km/h : </td>
+							<td td class="descrip">&nbsp&nbsp3.4s</td>
+						</tr>
+                        <tr>
+							<td class="topic">Chassis :  </td>
+							<td class="descrip">&nbsp Titanium & Carbonfibre </td>
+						</tr>
+                        <tr>
+							<td class="topic">No of Doors : </td>
+							<td td class="descrip">&nbsp&nbsp2</td>
+						</tr>
+                        <tr>
+							<td class="topic">Weight :  </td>
+							<td class="descrip">&nbsp 1235kg</td>
+						</tr>
+						<tr>
+							<td class="topic">Length :  </td>
+							<td class="descrip">&nbsp 4599mm</td>
+						</tr>
+					</table>
+				</p>
+				<br><br>
+			</div>
+		</div>
+	</div>	
+</section>
+<!-- End blog Area -->
+<!-- Footer -->
+	
+<footer>
+    <div class="footer-top text-center">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-6 text-center">
+                    <h4 class="navbar-brand">DRIVEX</h4>
+                    <p>We provide information about all types of cars in the world to all car addictors. We provide information about classic cars, luxury cars, sports cars and new arrivals. Also we have a range of articles about cars as well as our social media pages provide you additional insights about all types of cars.</p>
+                    <div class="col-auto social-icons">
+                        <a href="#"><i class='fa fa-facebook-f'></i></a>
+                        <a href="#"><i class='fa fa-twitter'></i></a>
+                        <a href="#"><i class='fa fa-instagram'></i></a>
+                        
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer-bottom text-center">
+        <p class="mb-0"></p> Made With <i class="fa fa-heart-o" aria-hidden="true"></i> By ICONS
+            
+    </div>
+</footer>
+
+
+<script src="../js/jquery-3.2.1.min.js"></script>
+<script src="../styles/bootstrap4/popper.js"></script>
+<script src="../styles/bootstrap4/bootstrap.min.js"></script>
+<script src="../plugins/greensock/TweenMax.min.js"></script>
+<script src="../plugins/greensock/TimelineMax.min.js"></script>
+<script src="../plugins/scrollmagic/ScrollMagic.min.js"></script>
+<script src="../plugins/greensock/animation.gsap.min.js"></script>
+<script src="../plugins/greensock/ScrollToPlugin.min.js"></script>
+<script src="../plugins/OwlCarousel2-2.2.1/owl.carousel.js"></script>
+<script src="../plugins/easing/easing.js"></script>
+<script src="../plugins/parallax-js-master/parallax.min.js"></script>
+<script src="../js/about_custom.js"></script>
+
+
+</body>
+
+</html>
