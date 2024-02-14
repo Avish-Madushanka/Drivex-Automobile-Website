1126e76d93b9af5598f5c0e0bdb9744eb69dd7de
Avish Madushanka <avishmadushanka21@gmail.com>
2024-02-14 17:59:56 +0530

    Create Sport.php

---
+++ sports/Sport.php
@@ -0,0 +1,337 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Sports Cars</title>
+<link rel="icon" type="image/x-icon" href="/image/Drivex.png">
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="description" content="DriveX">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="../styles/bootstrap4/bootstrap.min.css">
+<link href="../plugins/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" type="text/css" href="../plugins/OwlCarousel2-2.2.1/owl.carousel.css">
+<link rel="stylesheet" type="text/css" href="../plugins/OwlCarousel2-2.2.1/owl.theme.default.css">
+<link rel="stylesheet" type="text/css" href="../plugins/OwlCarousel2-2.2.1/animate.css">
+<link rel="stylesheet" type="text/css" href="../styles/luxury.css">
+<link rel="stylesheet" type="text/css" href="../styles/about_responsive.css">
+
+<style>
+
+@media screen and (max-width: 992px) {
+  .portfolio-block{
+    width:280px;
+    
+  }
+}
+
+@media screen and (max-width: 600px) {
+  .portfolio-block {
+    width: 280px;
+  }
+  }
+@media screen and (max-width: 992px) {
+  .img-responsive{
+    width: 100px;
+    height : 100px;
+  }
+}
+
+
+@media screen and (max-width: 600px) {
+  .img-responsive {
+    width: 100px;
+    height:100px
+}
+}
+</style>
+</head>
+
+<body>
+
+<div class="super_container">
+	
+	<!-- Header -->
+
+	<header class="header">
+
+
+		<!-- Main Navigation -->
+
+		
+		<nav class="main_nav">
+			<div class="container">
+				<div class="row">
+					<div class="col main_nav_col d-flex flex-row align-items-center justify-content-start">
+						<div class="logo_container">
+							<div class="logo"><img src="../image/Drivex.png" alt=""></div>
+						</div>
+						<div class="main_nav_container ml-auto">
+							<ul class="main_nav_list">
+								<li class="main_nav_item"><a href="../index.php">home</a></li>
+								<li class="main_nav_item"><a href="../about.php">about us</a></li>
+								<li class="main_nav_item"><a href="../blog.php">blog</a></li>
+								<li class="main_nav_item"><a href="../index.php#contact">Our Team</a></li>
+								<li class="main_nav_item"><a href="../contact.php">contact us</a></li>
+							</ul>
+						</div>
+						<div >
+							
+								<g>
+									<g>
+										<g>
+											<path class="mag_glass" fill="#FFFFFF" d="M78.438,216.78c0,57.906,22.55,112.343,63.493,153.287c40.945,40.944,95.383,63.494,153.287,63.494
+											s112.344-22.55,153.287-63.494C489.451,329.123,512,274.686,512,216.78c0-57.904-22.549-112.342-63.494-153.286
+											C407.563,22.549,353.124,0,295.219,0c-57.904,0-112.342,22.549-153.287,63.494C100.988,104.438,78.439,158.876,78.438,216.78z
+											M119.804,216.78c0-96.725,78.69-175.416,175.415-175.416s175.418,78.691,175.418,175.416
+											c0,96.725-78.691,175.416-175.416,175.416C198.495,392.195,119.804,313.505,119.804,216.78z"/>
+										</g>
+									</g>
+									<g>
+										<g>
+											<path class="mag_glass" fill="#FFFFFF" d="M6.057,505.942c4.038,4.039,9.332,6.058,14.625,6.058s10.587-2.019,14.625-6.058L171.268,369.98
+											c8.076-8.076,8.076-21.172,0-29.248c-8.076-8.078-21.172-8.078-29.249,0L6.057,476.693
+											C-2.019,484.77-2.019,497.865,6.057,505.942z"/>
+										</g>
+									</g>
+								</g>
+							</svg>
+						</div>
+
+						
+
+						<div class="hamburger">
+							<i class="fa fa-bars trans_200"></i>
+						</div>
+					</div>
+				</div>
+			</div>	
+		
+		</nav>
+
+	</header>
+	<div class="menu trans_500">
+		<div class="menu_content d-flex flex-column align-items-center justify-content-center text-center">
+			<div class="menu_close_container"><div class="menu_close"></div></div>
+			<div class="logo menu_logo"><img src="../image/Drivex.png" alt=""></div>
+			<ul>
+				<li class="menu_item"><a href="../index.php">home</a></li>
+				<li class="menu_item"><a href="../about.php">about us</a></li>
+				<li class="menu_item"><a href="../blog.php">blog</a></li>
+				<li class="menu_item"><a href="../index.php#contact">Our Team</a></li>
+				<li class="menu_item"><a href="../contact.php">contact us</a></li>
+			</ul>
+		</div>
+	</div>
+	
+
+	<!-- Intro -->
+
+	<div class="intro">
+		<div class="container">
+		
+				</div>
+				
+					<div class="intro_content">
+						<div class="intro_title"><center>Sport Car Brands</center></div>
+						
+						<div><div></div><span></span><span></span><span></span></a></div>
+					</div>
+				</div>
+				
+
+			
+
+	
+
+
+	<div class="row portfolio-items-wrapper">
+        <div class="mix col-md-4 design">
+            <div class="portfolio-block">
+                    <img class="img-responsive" src="../image/lambog.png">
+                    <div class="caption">
+                        <a class="search-icon image-popup" data-effect="mfp-with-zoom" href="Lambo.php" data-lightbox="image-1">
+                            <i class="fa fa-search"></i>
+                        </a>
+                        <h4><a href="">Lamborgini</a></h4>
+                        
+                    </div>
+                </div>
+                </div>
+        <div class="mix col-md-4 design">
+            <div class="portfolio-block">
+                    <img class="img-responsive" src="../image/ferrari.png"  alt="">
+                    <div class="caption">
+                        <a class="search-icon image-popup" data-effect="mfp-with-zoom" href="Ferrari.php" data-lightbox="image-1">
+                            <i class="fa fa-search"></i>
+                        </a>
+                        <h4><a href="">Ferrari</a></h4>
+                        
+                    </div>
+                </div>
+        </div>
+        <div class="mix col-md-4 design development">
+            <div class="portfolio-block">
+                    <img class="img-responsive" src="../image/bugatti.png" alt="">
+                    <div class="caption">
+                        <a class="search-icon image-popup" data-effect="mfp-with-zoom" href="Bugatti.php" data-lightbox="image-1">
+                            <i class="fa fa-search"></i>
+                        </a>
+                        <h4><a href="">Bugatti</a></h4>
+                        
+                    </div>
+                </div>
+        </div>
+        <div class="mix col-md-4 photography development">
+            <div class="portfolio-block">
+                    <img class="img-responsive" src="../image/pagani.png" alt="">
+                    <div class="caption">
+                        <a class="search-icon image-popup" data-effect="mfp-with-zoom" href="Pagani.php" data-lightbox="image-1">
+                            <i class="fa fa-search"></i>
+                        </a>
+                        <h4><a href="">Pagani</a></h4>
+                    
+                    </div>
+                </div>
+        </div>
+        <div class="mix col-md-4 photography development">
+            <div class="portfolio-block">
+                    <img class="img-responsive" src="../image/Koeng.png" alt="">
+                    <div class="caption">
+                        <a class="search-icon image-popup" data-effect="mfp-with-zoom" href="koneg.php" data-lightbox="image-1">
+                            <i class="fa fa-search"></i>
+                        </a>
+                        <h4><a href="">Koenigsegg</a></h4>
+                    
+                    </div>
+                </div>
+        </div>
+        <div class="mix col-md-4 photography" >
+            <div class="portfolio-block">
+                    <img class="img-responsive" src="../image/porche.png" alt="">
+                    <div class="caption">
+                        <a class="search-icon image-popup" data-effect="mfp-with-zoom" href="porschesp.php" data-lightbox="image-1">
+                            <i class="fa fa-search"></i>
+                        </a>
+                        <h4><a href="">Porsche</a></h4>
+                        
+                    </div>
+                </div>
+        </div>
+        <div class="mix col-md-4 design">
+            <div class="portfolio-block">
+                    <img class="img-responsive" src="../image/Aston.png" alt="">
+                    <div class="caption">
+                        <a class="search-icon image-popup" data-effect="mfp-with-zoom" href="aston.php" data-lightbox="image-1">
+                            <i class="fa fa-search"></i>
+                        </a>
+                        <h4><a href="">Aston-Martin</a></h4>
+                    </div>
+                </div>
+        </div>
+        <div class="mix col-md-4 design">
+            <div class="portfolio-block">
+                    <img class="img-responsive" src="../image/nissan.png">
+                    <div class="caption">
+                        <a class="search-icon image-popup" data-effect="mfp-with-zoom" href="nissansp.php" data-lightbox="image-1">
+                            <i class="fa fa-search"></i>
+                        </a>
+                        <h4><a href="">Nissan</a></h4>
+                        
+                    </div>
+                </div>
+        </div>
+        <div class="mix col-md-4  photography development">
+            <div class="portfolio-block">
+                <img class="img-responsive" src="../image/bmw.png">
+                <div class="caption">
+                    <a class="search-icon image-popup" data-effect="mfp-with-zoom" href="bmwsp.php" data-lightbox="image-1">
+                        <i class="fa fa-search"></i>
+                    </a>
+                    <h4><a href="">BMW</a></h4>
+                    
+                </div>
+            </div>
+        </div>
+        <div class="mix col-md-4  photography development">
+            <div class="portfolio-block">
+                <img class="img-responsive" src="../image/mass2.png">
+                <div class="caption">
+                    <a class="search-icon image-popup" data-effect="mfp-with-zoom" href="maseratisp.php" data-lightbox="image-1">
+                        <i class="fa fa-search"></i>
+                    </a>
+                    <h4><a href="">Maserati</a></h4>
+                    
+                </div>
+            </div>
+        </div>
+        <div class="mix col-md-4  photography development">
+            <div class="portfolio-block">
+                <img class="img-responsive" src="../image/audi.png" alt="">
+                <div class="caption">
+                    <a class="search-icon image-popup" data-effect="mfp-with-zoom" href="audisp.php" data-lightbox="image-1">
+                        <i class="fa fa-search"></i>
+                    </a>
+                    <h4><a href="">Audi</a></h4>
+                    
+                </div>
+            </div>
+        </div>
+      
+        <div class="mix col-md-4  photography development">
+            <div class="portfolio-block">
+                <img class="img-responsive" src="../image/mclaren.png">
+                <div class="caption">
+                    <a class="search-icon image-popup" data-effect="mfp-with-zoom" href="mclarensp.php" data-lightbox="image-1">
+                        <i class="fa fa-search"></i>
+                    </a>
+                    <h4><a href="">Mclaren</a></h4>
+                    
+                </div>
+            </div>
+        </div>
+ </div>
+</div>
+	<!-- Footer -->
+	
+	<footer>
+        <div class="footer-top text-center">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-6 text-center">
+                        <h4 class="navbar-brand">DRIVEX</span></h4>
+                        <p>We provide information about all types of cars in the world to all car addictors. We provide information about classic cars, luxury cars, sports cars, electric cars and new arrivals. Also we have a range of articles about cars as well as our social media pages provide you additional insights about all types of cars.</p>
+                        <div class="col-auto social-icons">
+                            <a href="#"><i class='fa fa-facebook-f'></i></a>
+                            <a href="#"><i class='fa fa-twitter'></i></a>
+                            <a href="#"><i class='fa fa-instagram'></i></a>
+                            
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer-bottom text-center">
+            <p class="mb-0"></p> Made With <i class="fa fa-heart-o" aria-hidden="true"></i> By ICONS
+                
+        </div>
+    </footer>
+
+
+    <script src="../js/jquery-3.2.1.min.js"></script>
+<script src="../styles/bootstrap4/popper.js"></script>
+<script src="../styles/bootstrap4/bootstrap.min.js"></script>
+<script src="../plugins/greensock/TweenMax.min.js"></script>
+<script src="../plugins/greensock/TimelineMax.min.js"></script>
+<script src="../plugins/scrollmagic/ScrollMagic.min.js"></script>
+<script src="../plugins/greensock/animation.gsap.min.js"></script>
+<script src="../plugins/greensock/ScrollToPlugin.min.js"></script>
+<script src="../plugins/OwlCarousel2-2.2.1/owl.carousel.js"></script>
+<script src="../plugins/easing/easing.js"></script>
+<script src="../plugins/parallax-js-master/parallax.min.js"></script>
+<script src="../js/about_custom.js"></script>
+
+
+</body>
+
+</html>
