19ff2603a49fb425dcee4d2212870b0d1c99a251
Avish Madushanka <avishmadushanka21@gmail.com>
2024-02-14 18:01:22 +0530

    Create header.php

---
+++ sports/header.php
@@ -0,0 +1,73 @@
+<html>
+
+
+
+		<!-- Main Navigation -->
+
+		
+		<nav class="main_nav">
+			<div class="container">
+				<div class="row">
+					<div class="col main_nav_col d-flex flex-row align-items-center justify-content-start">
+						<div class="logo_container">
+							<div class="logo"><img src="../image/Drivex.png" alt=""></div>
+						</div>
+						<div class="main_nav_container ml-auto">
+							<ul class="main_nav_list">
+								<li class="main_nav_item"><a href="../index.php">home</a></li>
+								<li class="main_nav_item"><a href="../about.php">about us</a></li>
+								<li class="main_nav_item"><a href="../blog.php">blog</a></li>
+								<li class="main_nav_item"><a href="../index.php#contact">Our Team</a></li>
+								<li class="main_nav_item"><a href="../contact.php">contact us</a></li>
+							</ul>
+						</div>
+						<div >
+							
+								<g>
+									<g>
+										<g>
+											<path class="mag_glass" fill="#FFFFFF" d="M78.438,216.78c0,57.906,22.55,112.343,63.493,153.287c40.945,40.944,95.383,63.494,153.287,63.494
+											s112.344-22.55,153.287-63.494C489.451,329.123,512,274.686,512,216.78c0-57.904-22.549-112.342-63.494-153.286
+											C407.563,22.549,353.124,0,295.219,0c-57.904,0-112.342,22.549-153.287,63.494C100.988,104.438,78.439,158.876,78.438,216.78z
+											M119.804,216.78c0-96.725,78.69-175.416,175.415-175.416s175.418,78.691,175.418,175.416
+											c0,96.725-78.691,175.416-175.416,175.416C198.495,392.195,119.804,313.505,119.804,216.78z"/>
+										</g>
+									</g>
+									<g>
+										<g>
+											<path class="mag_glass" fill="#FFFFFF" d="M6.057,505.942c4.038,4.039,9.332,6.058,14.625,6.058s10.587-2.019,14.625-6.058L171.268,369.98
+											c8.076-8.076,8.076-21.172,0-29.248c-8.076-8.078-21.172-8.078-29.249,0L6.057,476.693
+											C-2.019,484.77-2.019,497.865,6.057,505.942z"/>
+										</g>
+									</g>
+								</g>
+							</svg>
+						</div>
+
+						
+
+						<div class="hamburger">
+							<i class="fa fa-bars trans_200"></i>
+						</div>
+					</div>
+				</div>
+			</div>	
+		
+		</nav>
+
+	</header>
+	<div class="menu trans_500">
+		<div class="menu_content d-flex flex-column align-items-center justify-content-center text-center">
+			<div class="menu_close_container"><div class="menu_close"></div></div>
+			<div class="logo menu_logo"><img src="../image/Drivex.png" alt=""></div>
+			<ul>
+				<li class="menu_item"><a href="../index.php">home</a></li>
+				<li class="menu_item"><a href="../about.php">about us</a></li>
+				<li class="menu_item"><a href="../blog.php">blog</a></li>
+				<li class="menu_item"><a href="../index.php#contact">Our Team</a></li>
+				<li class="menu_item"><a href="../contact.php">contact us</a></li>
+			</ul>
+		</div>
+	</div>
+
+</html>
